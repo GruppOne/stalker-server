6189a1ac2dce14cfd6f9943115e39acf299e270b
erclu <30255227+erclu@users.noreply.github.com>
2020-03-30 12:38:28 +0200

    chore: add release scripts and markdown lint workflow

---
+++ .release/tag-current.ps1
@@ -0,0 +1,15 @@
+if (($args -match " -h") -or ($args -match " --help")) {
+  npx standard-version -h
+  return
+}
+
+Write-Output "Tagging current release..."
+
+npx standard-version --skip.bump --skip.changelog --skip.commit $args
+
+if ($LASTEXITCODE) {
+  Write-Error "Something wrong with standard-version"
+}
+else {
+  Write-Output "All good! you can push the generated tag."
+}
