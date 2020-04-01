e4228f89fa6ae366ebac7d9f678c058fbf586aef
erclu <30255227+erclu@users.noreply.github.com>
2020-04-01 17:44:54 +0200

    feat: add project generated with spring initializr
    generation link:
    https://start.spring.io/#!type=gradle-project&language=java&platformVersion=2.2.6.RELEASE&packaging=jar&jvmVersion=11&groupId=tech.gruppone&artifactId=stalker-server&name=stalker-server&description=Backend%20for%20Stalker%20(SWE%20project%20%40%20CS%20course%20UniPD&packageName=tech.gruppone.stalker-server&dependencies=devtools,lombok,configuration-processor,webflux,data-rest,session,data-rest-hal,data-jpa,mail,restdocs,unboundid-ldap

---
+++ src/main/java/tech/gruppone/stalkerserver/StalkerServerApplication.java
@@ -0,0 +1,13 @@
+package tech.gruppone.stalkerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StalkerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StalkerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/gruppone/stalkerserver/StalkerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.gruppone.stalkerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StalkerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
