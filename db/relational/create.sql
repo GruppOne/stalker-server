cd3a7a7a3f108a394439dd857ce3ab75b3f155ce
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-04-04 15:57:34 +0200

    feat: initial configuration (#2)
    * feat: add DockerFile and docker-compose for server
    
    * chore: add some missing settings
    
    * docs: add fields set schema for timeseries db
    
    * fix: fix environment part in docker-compose
    
    * feat: add influxdb script and influxdb configuration
    
    README updated
    
    * fix: fix name file .env.example in .env
    
    * chore: streamline some settings in containers and add .env to gitignore
    
    * build: prepare gitignore for gradle-based spring project
    
    standardize service structure in docker-compose
    
    * chore: add several improvements to sql file
    
    Added statement for database creation, modified some fields in NOT NULL to improve indexing, added
    AUTO_INCREMENT attribute to the columns that required it
    
    * chore: update extension list and change commitizen adapter
    
    * chore: change data types of date columns to datetime
    
    change data types of date columns from timestamp to datetime according with objects data type in web
    app
    
    * chore: add release scripts and markdown lint workflow
    
    * build: final touches to configuration
    
    * feat: add project generated with spring initializr
    
    generation link:
    https://start.spring.io/#!type=gradle-project&language=java&platformVersion=2.2.6.RELEASE&packaging=jar&jvmVersion=11&groupId=tech.gruppone&artifactId=stalker-server&name=stalker-server&description=Backend%20for%20Stalker%20(SWE%20project%20%40%20CS%20course%20UniPD&packageName=tech.gruppone.stalker-server&dependencies=devtools,lombok,configuration-processor,webflux,data-rest,session,data-rest-hal,data-jpa,mail,restdocs,unboundid-ldap
    
    * style: convert tabs to spaces
    
    * build: remove excess dependencies and configure releaser
    
    * chore: rename a bunch of files and commit .idea config
    
    * docs: change influxdb schema and add comments
    
    * refactor: standardize db table and column names
    
    * chore: some changes to configuration
    
    * chore: improve releaser scripts
    
    * build(gradle): remove non-essential dependencies
    
    * feat: implement version endpoint
    
    * chore: fix releaser script
    
    * build(docker): switch back to mysql and fix docker errors
    
    * chore: add default NULL to Organizations.ldapConfiguration
    
    * chore(release): 0.1.0
    
    Co-authored-by: AlbertoGobbo <47864124+AlbertoGobbo@users.noreply.github.com>
    Co-authored-by: Fabio Scettro <fabioscettro@gmail.com>

---
+++ db/relational/create.sql
@@ -0,0 +1,359 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mar 16, 2020 alle 13:09
+-- Versione del server: 10.1.37-MariaDB
+-- Versione PHP: 7.2.12
+-- FIXME should dump this from mysql!
+
+SET
+  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+SET
+  AUTOCOMMIT = 0;
+
+START TRANSACTION;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+;
+
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+;
+
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+;
+
+/*!40101 SET NAMES utf8mb4 */
+;
+
+-- --------------------------------------------------------
+--
+-- Database: `stalker-rdb`
+--
+CREATE DATABASE IF NOT EXISTS `stalker-rdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+USE `stalker-rdb`;
+
+-- --------------------------------------------------------
+--
+-- Struttura della tabella `AdminType`
+--
+CREATE TABLE `AdminType` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+-- --------------------------------------------------------
+--
+-- Struttura della tabella `Connections`
+--
+CREATE TABLE `Connections` (
+  `organizationId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `createdDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+-- --------------------------------------------------------
+--
+-- Struttura della tabella `LdapConfiguration`
+--
+CREATE TABLE `LdapConfiguration` (
+  `id` int(11) NOT NULL,
+  `host` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` char(64) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+-- --------------------------------------------------------
+--
+-- Struttura della tabella `OrganizationRole`
+--
+CREATE TABLE `OrganizationRole` (
+  `organizationId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `adminType` int(11) NOT NULL,
+  `createdDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+-- --------------------------------------------------------
+--
+-- Struttura della tabella `Organizations`
+--
+CREATE TABLE `Organizations` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` tinytext NOT NULL,
+  `ldapConf` int(11) NULL DEFAULT NULL,
+  `createdDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `lastModifiedDate` DATETIME NULL DEFAULT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+-- --------------------------------------------------------
+--
+-- Struttura della tabella `PlaceData`
+--
+CREATE TABLE `PlaceData` (
+  `id` int(11) NOT NULL,
+  `address` varchar(150) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `zipcode` varchar(10) NOT NULL,
+  `state` varchar(50) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+-- --------------------------------------------------------
+--
+-- Struttura della tabella `Place`
+--
+CREATE TABLE `Place` (
+  `id` int(11) NOT NULL,
+  `organizationId` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `position` linestring NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+-- --------------------------------------------------------
+--
+-- Struttura della tabella `UserData`
+--
+CREATE TABLE `UserData` (
+  `userId` int(11) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `birthDate` date NOT NULL,
+  `createdDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `lastModifiedDate` DATETIME NULL DEFAULT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+-- --------------------------------------------------------
+--
+-- Struttura della tabella `UserLog`
+--
+CREATE TABLE `UserLog` (
+  `userId` int(11) NOT NULL,
+  `ip` varchar(50) NOT NULL,
+  `createdDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+-- --------------------------------------------------------
+--
+-- Struttura della tabella `Users`
+--
+CREATE TABLE `Users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` char(64) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+--
+-- Indici per le tabelle scaricate
+--
+--
+-- Indici per le tabelle `AdminType`
+--
+ALTER TABLE
+  `AdminType`
+ADD
+  PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `Connections`
+--
+ALTER TABLE
+  `Connections`
+ADD
+  KEY `organizationId` (`organizationId`),
+ADD
+  KEY `userId` (`userId`);
+
+--
+-- Indici per le tabelle `LdapConfiguration`
+--
+ALTER TABLE
+  `LdapConfiguration`
+ADD
+  PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `OrganizationRole`
+--
+ALTER TABLE
+  `OrganizationRole`
+ADD
+  KEY `organizationId` (`organizationId`),
+ADD
+  KEY `userId` (`userId`),
+ADD
+  KEY `adminType` (`adminType`);
+
+--
+-- Indici per le tabelle `Organizations`
+--
+ALTER TABLE
+  `Organizations`
+ADD
+  PRIMARY KEY (`id`),
+ADD
+  KEY `ldapConf` (`ldapConf`);
+
+--
+-- Indici per le tabelle `PlaceData`
+--
+ALTER TABLE
+  `PlaceData`
+ADD
+  KEY `id` (`id`);
+
+--
+-- Indici per le tabelle `Place`
+--
+ALTER TABLE
+  `Place`
+ADD
+  PRIMARY KEY (`id`),
+ADD
+  KEY `organizationId` (`organizationId`);
+
+--
+-- Indici per le tabelle `UserData`
+--
+ALTER TABLE
+  `UserData`
+ADD
+  KEY `userId` (`userId`);
+
+--
+-- Indici per le tabelle `UserLog`
+--
+ALTER TABLE
+  `UserLog`
+ADD
+  KEY `userId` (`userId`);
+
+--
+-- Indici per le tabelle `Users`
+--
+ALTER TABLE
+  `Users`
+ADD
+  PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+--
+-- AUTO_INCREMENT per la tabella `AdminType`
+--
+ALTER TABLE
+  `AdminType`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `LdapConfiguration`
+--
+ALTER TABLE
+  `LdapConfiguration`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `Organizations`
+--
+ALTER TABLE
+  `Organizations`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `Place`
+--
+ALTER TABLE
+  `Place`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `Users`
+--
+ALTER TABLE
+  `Users`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+--
+-- Limiti per la tabella `Connections`
+--
+ALTER TABLE
+  `Connections`
+ADD
+  CONSTRAINT `Connections_ibfk_1` FOREIGN KEY (`organizationId`) REFERENCES `Organizations` (`id`),
+ADD
+  CONSTRAINT `Connections_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `Users` (`id`);
+
+--
+-- Limiti per la tabella `OrganizationRole`
+--
+ALTER TABLE
+  `OrganizationRole`
+ADD
+  CONSTRAINT `OrganizationRole_ibfk_1` FOREIGN KEY (`organizationId`) REFERENCES `Organizations` (`id`),
+ADD
+  CONSTRAINT `OrganizationRole_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `Users` (`id`),
+ADD
+  CONSTRAINT `OrganizationRole_ibfk_3` FOREIGN KEY (`adminType`) REFERENCES `AdminType` (`id`);
+
+--
+-- Limiti per la tabella `Organizations`
+--
+ALTER TABLE
+  `Organizations`
+ADD
+  CONSTRAINT `Organizations_ibfk_1` FOREIGN KEY (`ldapConf`) REFERENCES `LdapConfiguration` (`id`);
+
+--
+-- Limiti per la tabella `PlaceData`
+--
+ALTER TABLE
+  `PlaceData`
+ADD
+  CONSTRAINT `PlaceData_ibfk_1` FOREIGN KEY (`id`) REFERENCES `Place` (`id`);
+
+--
+-- Limiti per la tabella `Place`
+--
+ALTER TABLE
+  `Place`
+ADD
+  CONSTRAINT `Place_ibfk_1` FOREIGN KEY (`organizationId`) REFERENCES `Organizations` (`id`);
+
+--
+-- Limiti per la tabella `UserData`
+--
+ALTER TABLE
+  `UserData`
+ADD
+  CONSTRAINT `UserData_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `Users` (`id`);
+
+--
+-- Limiti per la tabella `UserLog`
+--
+ALTER TABLE
+  `UserLog`
+ADD
+  CONSTRAINT `UserLog_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `Users` (`id`);
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+;
+
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+;
+
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+;
