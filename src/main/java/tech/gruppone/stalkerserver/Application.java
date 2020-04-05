b79bf27f1539dcebb07f7b7eb4d5f39e344ec08b
AlbertoGobbo <47864124+AlbertoGobbo@users.noreply.github.com>
2020-04-05 02:04:53 +0200

    build(gradle): add dependencies for influxdb

--- src/main/java/tech/gruppone/stalkerserver/Application.java
+++ src/main/java/tech/gruppone/stalkerserver/Application.java
@@ -2,9 +2,11 @@ package tech.gruppone.stalkerserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+@CrossOrigin
 @SpringBootApplication
 @EnableWebFlux
 @RestController
