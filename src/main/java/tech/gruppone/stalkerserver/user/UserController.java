540340048bf3a6d0dd720d0287bad94e97b96cdb
Alberto Cocco <albertococco98@gmail.com>
2020-04-07 00:46:57 +0200

    feat: add login endpoint

--- src/main/java/tech/gruppone/stalkerserver/user/UserController.java
+++ src/main/java/tech/gruppone/stalkerserver/user/UserController.java
@@ -3,15 +3,23 @@ package tech.gruppone.stalkerserver.user;
 //import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 //import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Mono;
 //import reactor.core.publisher.Mono;
 
 @RequestMapping("/users")
 @Controller
 public class UserController {
+
+  UserRepository userRepo;
   private static Logger logger = LoggerFactory.getLogger(UserController.class);
 
   /*
@@ -21,7 +29,18 @@ public class UserController {
     String password = body.get("password");
   }
   */
+  @PostMapping("/login")
+  public Mono<ResponseEntity<User>> login (@RequestBody UnauthenticatedUser user){
+    if(userRepo.findByEmail(user.getEmail()) && userRepo.finByPassword(user.getPassword())){
+        logger.info("user is logged");
+        return Mono.just(ResponseEntity.status(HttpStatus.ACCEPTED).build());
+    }
+    else{
+       logger.info("user is not logged");
+       return Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+    }
 
+  }
   /*
   @GetMapping("users/{id}/connected")
   public Type nameMethod(@PathVariable int id){}
