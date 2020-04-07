3a63e02ab433da6ac42781dbba81ad9b3f32b9b4
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-04-07 04:47:20 +0200

    feat: implement version endpoint

--- src/main/java/tech/gruppone/stalkerserver/configuration/ApplicationConfiguration.java
+++ src/main/java/tech/gruppone/stalkerserver/configuration/ApplicationConfiguration.java
@@ -1,19 +1,30 @@
 package tech.gruppone.stalkerserver.configuration;
 
 import lombok.Getter;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @PropertySource("classpath:application.properties")
+@RestController
 @Getter
 public class ApplicationConfiguration {
 
+  @NonNull
   private final String version;
 
   public ApplicationConfiguration(@Value("${spring.application.version}") String version) {
     this.version = version;
   }
 
+  @GetMapping("/version")
+  public Mono<String> currentServerVersion() {
+
+    return Mono.just(version);
+  }
 }
