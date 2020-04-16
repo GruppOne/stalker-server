455a55548a652f254faba603aed5b279ad48c4dc
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-04-16 19:44:27 +0000

    feat: configure relational db with async driver

--- src/main/java/tech/gruppone/stalker/server/configuration/ApplicationConfiguration.java
+++ src/main/java/tech/gruppone/stalker/server/configuration/ApplicationConfiguration.java
@@ -1,18 +1,15 @@
 package tech.gruppone.stalker.server.configuration;
 
-import lombok.Getter;
-import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.NonNull;
 import reactor.core.publisher.Mono;
 
-// TODO refactor using @configurationproperties for typed config values?
 @Configuration
 @PropertySource("classpath:application.properties")
-@Getter
 @RestController
 public class ApplicationConfiguration {
 
@@ -22,12 +19,11 @@ public class ApplicationConfiguration {
     this.version = version;
   }
 
-  @GetMapping(value = {"/", "/version"})
+  @GetMapping("/version")
   public Mono<String> currentServerVersion() {
 
-    // FIXME should not be done like this
-    final String versionObject = "{\"version\":\"" + version + "\"}";
-    return Mono.just(versionObject);
+    // FIXME should return an application/json made like {"version":...}
+    return Mono.just(version);
   }
 
   // TODO inner class named versioninfo. move restcontroller there
