8e3e438f38d380d3a0b9da00d9b03e0509dc112b
Fabio Scettro <fabioscettro@gmail.com>
2020-04-16 16:36:07 +0200

    refactor: change names to camelcase

--- db/relational/test-data.sql
+++ db/relational/test-data.sql
@@ -10,7 +10,6 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -26,7 +25,7 @@ SET time_zone = "+00:00";
 -- Dump dei dati per la tabella `organizations`
 --
 
-INSERT INTO `organizations` (`name`, `description`, `ldap_conf`, `created_date`, `last_modified_date`) VALUES
+INSERT INTO `Organizations` (`name`, `description`, `ldapConf`, `createdDate`, `lastModifiedDate`) VALUES
 ('GruppOne', 'Organizzazione del gruppo 1 per il progetto di Ingegneria del Software', NULL, '2020-04-16 14:48:26', NULL),
 ('Amici della Torre Archimede', 'Organizzazione di prova, è un gruppo di amici di Informatica ☺️👨🏻‍💻👩🏿‍💻', NULL, '2020-04-16 14:49:58', NULL);
 
@@ -36,7 +35,7 @@ INSERT INTO `organizations` (`name`, `description`, `ldap_conf`, `created_date`,
 SET @g = 'POLYGON((45.408351 11.886792,45.408164 11.886736,45.408115 11.887098,45.408351 11.886792))';
 SET @h = 'POLYGON((45.408194 11.886379,45.408074 11.886312,45.408063 11.88635,45.408021 11.886304,45.408051 11.886208,45.407978 11.88617,45.407938 11.886283,45.407857 11.886275,45.407859 11.886347,45.407935 11.88635,45.407906 11.886535,45.407859 11.886532,45.407846 11.886092,45.407711 11.886106,45.407718 11.886583,45.407703 11.886878,45.408051 11.886988,45.408074 11.88691,45.408106 11.886926,45.40813 11.886819,45.408194 11.886379))';
 SET @j = 'POLYGON((45.411561 11.887471, 45.411224 11.887326,45.411111 11.887785,45.411561 11.887471))';
-INSERT INTO `place` (`organization_id`, `name`, `position`) VALUES
+INSERT INTO `Place` (`organizationId`, `name`, `position`) VALUES
 (1, 'Aule Luzzati', PolygonFromText(@g)),
 (1, 'Complesso Paolotti', PolygonFromText(@h)),
 (2, 'Torre Archimede', PolygonFromText(@j));
@@ -45,7 +44,7 @@ INSERT INTO `place` (`organization_id`, `name`, `position`) VALUES
 -- Dump dei dati per la tabella `placedata`
 --
 
-INSERT INTO `placedata` (`id`, `address`, `city`, `zipcode`, `state`) VALUES
+INSERT INTO `PlaceData` (`id`, `address`, `city`, `zipcode`, `state`) VALUES
 (1, 'Via Paolotti', 'Padova', '35131', 'Italia'),
 (2, 'Via Paolotti', 'Padova', '35131', 'Italia'),
 (3, 'Via Trieste', 'Padova', '35131', 'Italia');
@@ -54,7 +53,7 @@ INSERT INTO `placedata` (`id`, `address`, `city`, `zipcode`, `state`) VALUES
 -- Dump dei dati per la tabella `users`
 --
 
-INSERT INTO `users` (`email`, `password`) VALUES
+INSERT INTO `Users` (`email`, `password`) VALUES
 ('mariotest01@gmail.com', 'f853a81c51cdc3b75d5af43379965c56ff55a6fcf67d7cbc5daca8e1f7db01df46b768a35d8e472abda7a9ecc5bc46da4d56c4c85658ebd3557d6d08225a2352'),
 ('giorgiotest02@hotmail.it', '95f9d376cada5e12dac91abb345c8d1d53f30fd7469a144796d547ca73db1ceb05aadbd7ba0d4ea36c57e5e5c30098d13140beb93d306b0bf73d25a9a5281cbd'),
 ('mariatest03@live.it', 'ba191a9e86125cacf7dfe63e97728b88dfac7e1b6b90b853dbc6677cfdacf241630ba438d3d7446d7d781417aa1956ecd68d651a8da4523b134144e6ccb0a531'),
@@ -65,7 +64,7 @@ INSERT INTO `users` (`email`, `password`) VALUES
 -- Dump dei dati per la tabella `userdata`
 --
 
-INSERT INTO `userdata` (`user_id`, `firstname`, `lastname`, `birth_date`, `created_date`, `last_modified_date`) VALUES
+INSERT INTO `UserData` (`userId`, `firstName`, `lastName`, `birthDate`, `createdDate`, `lastModifiedDate`) VALUES
 (1, 'Mario', 'Rossi', '1960-04-16', '2020-04-16 14:39:08', NULL),
 (2, 'Giorgio', 'Bianchi', '1983-11-21', '2020-04-16 14:39:42', NULL),
 (3, 'Maria', 'Verdi', '1987-08-14', '2020-04-16 14:40:03', NULL),
