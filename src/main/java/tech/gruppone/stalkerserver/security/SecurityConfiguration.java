c702141a136fd1522fc51789cc2534de05a5295a
Alberto Cocco <albertococco98@gmail.com>
2020-04-21 10:36:40 +0200

    fix: resolve conflicts

---
+++ src/main/java/tech/gruppone/stalkerserver/security/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.example.Stalkerserver.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+@PropertySource("classpath:application.properties")
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+  @Value("${my.api.key}")
+  private String api_key;
+
+  @Value("${my.api.value}")
+  private String api_value;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception{
+    ApiKeyAuthenticationFilter filter  = new ApiKeyAuthenticationFilter(api_key);
+    filter.setAuthenticationManager(new AuthenticationManager() {
+
+      @Override
+      public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String principal = (String) authentication.getPrincipal();
+        if (!api_value.equals(principal))
+        {
+          throw new BadCredentialsException("The API key is not correct");
+        }
+        authentication.setAuthenticated(true);
+        return authentication;
+      }
+    });
+    http.
+      antMatcher("/**").
+      csrf().disable().                        //.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).
+      sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+      and().addFilter(filter).authorizeRequests().anyRequest().authenticated();
+  }
+
+
+
+}
