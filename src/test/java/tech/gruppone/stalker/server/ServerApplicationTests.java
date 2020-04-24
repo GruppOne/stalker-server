2295c68c111b4b5f0760cbbb2e95c5c1542dafcc
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-04-24 15:45:27 +0200

    test: write test to check that context loads correctly

--- src/test/java/tech/gruppone/stalker/server/ServerApplicationTests.java
+++ src/test/java/tech/gruppone/stalker/server/ServerApplicationTests.java
@@ -1,23 +1,20 @@
 package tech.gruppone.stalker.server;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-// XXX what does this do?
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ServerApplicationTests {
 
   @Autowired
   private WebTestClient webTestClient;
 
+  // TODO how to test that context loads correctly without requesting stuff from endpoints?
   @Test
   public void contextLoads() {
-    webTestClient.get().uri("/").exchange().expectStatus().isNotFound();
+    webTestClient.get().uri("/version").exchange().expectStatus().isOk();
   }
 
 }
