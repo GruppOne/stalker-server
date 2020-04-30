c6f6e4528b36bab157c64671f445cd53032a71c8
AlbertoGobbo <47864124+AlbertoGobbo@users.noreply.github.com>
2020-04-30 15:42:16 +0200

    feat: delete useless gradle dependence

--- src/main/java/tech/gruppone/stalker/server/ServerApplication.java
+++ src/main/java/tech/gruppone/stalker/server/ServerApplication.java
@@ -1,33 +1,21 @@
 package tech.gruppone.stalker.server;
 
-import javax.annotation.PostConstruct;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
+// import lombok.extern.slf4j.Slf4j;
+
 // TODO find a way to return 501 NOT IMPLEMENTED
-// TODO decide which logger we use. Should be used with the appropriate lombok @log annotation. will probably be @slf4j
 // TODO find an easy way to wrap values inside a valid json
+// @Slf4j
 @SpringBootApplication
 @EnableR2dbcRepositories
 public class ServerApplication {
 
-  @Autowired
-  private ObjectMapper objectMapper;
-
   public static void main(String[] args) {
     SpringApplication.run(ServerApplication.class, args);
 
-  }
-
-  @PostConstruct
-  public void setUp() {
-    objectMapper.registerModule(new JavaTimeModule());
   }
 
 }
