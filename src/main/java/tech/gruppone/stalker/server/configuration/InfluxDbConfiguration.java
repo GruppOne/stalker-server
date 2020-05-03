366655509d96097d25dafec1f07b576b2904da60
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-05-03 18:11:53 +0200

    feat: add correctly configured influxdb bean

--- src/main/java/tech/gruppone/stalker/server/configuration/InfluxDbConfiguration.java
+++ src/main/java/tech/gruppone/stalker/server/configuration/InfluxDbConfiguration.java
@@ -1,31 +1,67 @@
 package tech.gruppone.stalker.server.configuration;
 
-import lombok.Getter;
-import lombok.NonNull;
+import org.influxdb.BatchOptions;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDB.LogLevel;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Query;
+import org.influxdb.impl.InfluxDBMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.influx.InfluxDbAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import lombok.NonNull;
+import lombok.experimental.NonFinal;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Configuration
 @PropertySource("classpath:application.properties")
-// @PropertySource("classpath:local.properties")
-@Getter
-public class InfluxDbConfiguration {
+@lombok.Value
+@NonFinal
+public class InfluxDbConfiguration extends InfluxDbAutoConfiguration {
 
-  // TODO use constructor value injection
-  @NonNull
-  @Value("${spring.influx.url}")
-  private String url;
+  String url;
+  String database;
+  String retentionPolicy;
+  String username;
+  String password;
 
-  @NonNull
-  @Value("${spring.influx.database}")
-  private String databaseName;
+  public InfluxDbConfiguration(@NonNull @Value("${spring.influx.url}") String url,
+      @NonNull @Value("${spring.influx.database}") String database,
+      @NonNull @Value("${spring.influx.retention}") String retentionPolicy,
+      @NonNull @Value("${spring.influx.user}") String userName,
+      @NonNull @Value("${spring.influx.password}") String password) {
+    this.url = url;
+    this.database = database;
+    this.retentionPolicy = retentionPolicy;
+    this.username = userName;
+    this.password = password;
+  }
 
-  @NonNull
-  @Value("${spring.influx.user}")
-  private String userName;
+  // TODO handle errors when establishing the connection
+  @Bean(destroyMethod = "close")
+  public InfluxDB getInfluxDBConnection() {
+    log.info("creating connection to InfluxDB {} on db {}", url, database);
 
-  @NonNull
-  @Value("${spring.influx.password}")
-  private String password;
+    final var influxDB = InfluxDBFactory.connect(url, username, password).setDatabase(database)
+        .setRetentionPolicy(retentionPolicy).enableGzip().setLogLevel(LogLevel.BASIC)
+        .enableBatch(BatchOptions.DEFAULTS);
+
+    // XXX careful! this operation is blocking.
+    var queryResult = influxDB.query(new Query("SHOW DATABASES"));
+    var result = queryResult.getResults();
+
+    log.info(result);
+
+    return influxDB;
+  }
+
+  @Bean
+  public InfluxDBMapper getInfluxDBMapper() {
+    return new InfluxDBMapper(getInfluxDBConnection());
+  }
+
 }
