74f53c3edbff3596ac7351425fbba48f223914fb
AlbertoGobbo <gobboalberto98@gmail.com>
2020-05-13 19:07:50 +0200

    feat: add new features about /user/{userId} endpoint
    Pay attention: this endpoint doesn't work, the problem is localized into UserService.java. Please
    help us!

--- src/main/java/tech/gruppone/stalker/server/model/api/UserDataDto.java
+++ src/main/java/tech/gruppone/stalker/server/model/api/UserDataDto.java
@@ -2,16 +2,16 @@ package tech.gruppone.stalker.server.model.api;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.Builder;
 // import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 // import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 // import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 // import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
+@Builder
 @Data
-@NoArgsConstructor
 public class UserDataDto {
   @NonNull
   String email;
@@ -26,7 +26,6 @@ public class UserDataDto {
   // @JsonDeserialize(using = LocalDateDeserializer.class)
   // @JsonSerialize(using = LocalDateSerializer.class)
   LocalDate birthDate;
-
 
   LocalDateTime creationDateTime;
 
--- src/main/java/tech/gruppone/stalker/server/model/api/UserDto.java
+++ src/main/java/tech/gruppone/stalker/server/model/api/UserDto.java
@@ -1,12 +1,12 @@
 package tech.gruppone.stalker.server.model.api;
 
 import org.springframework.data.annotation.Id;
+import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
+@Builder
 @Data
-@NoArgsConstructor
 public class UserDto {
   @Id
   Long id;
