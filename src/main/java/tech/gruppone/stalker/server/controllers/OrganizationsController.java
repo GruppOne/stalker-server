b8cb5de4236bf35c676a9437fb1f50d2b3d73f50
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-05-13 11:17:58 +0200

    feat: implement get /organizations

--- src/main/java/tech/gruppone/stalker/server/controllers/OrganizationsController.java
+++ src/main/java/tech/gruppone/stalker/server/controllers/OrganizationsController.java
@@ -1,8 +1,8 @@
 package tech.gruppone.stalker.server.controllers;
 
+import java.util.List;
 import lombok.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -11,15 +11,18 @@ import tech.gruppone.stalker.server.repositories.OrganizationRepository;
 
 @Value
 @RestController
-@RequestMapping("/organization")
-public class OrganizationController {
+@RequestMapping("/organizations")
+public class OrganizationsController {
 
   OrganizationRepository organizationRepository;
 
-  @GetMapping("/{id}")
-  public Mono<Organization> getOrganizationById(@PathVariable Long id) {
+  @GetMapping
+  public Mono<OrganizationsResponse> getOrganizations() {
+    return organizationRepository.findAll().collectList().map(OrganizationsResponse::new);
+  }
 
-    // return organizationRepository.findById(id);
-    return Mono.error(RuntimeException::new);
+  @Value
+  private static class OrganizationsResponse {
+    List<Organization> organizations;
   }
 }
--- src/main/java/tech/gruppone/stalker/server/repositories/OrganizationRepository.java
+++ src/main/java/tech/gruppone/stalker/server/repositories/OrganizationRepository.java
@@ -4,14 +4,13 @@ import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import tech.gruppone.stalker.server.model.Organization;
+import tech.gruppone.stalker.server.model.db.Organization;
 
 public interface OrganizationRepository extends ReactiveCrudRepository<Organization,Long> {
 
-  @Query("select * from Organizations")
+  @Query("select * from Organization")
   public Flux<Organization> findAll();
 
-
-  @Query("select * from Organizations o where o.id = :id")
+  @Query("select * from Organization o where o.id = :id")
   public Mono<Organization> findById(Long id);
 }
