739484903806ba3fef3de295f8a1024a1bdcd16e
AlbertoGobbo <gobboalberto98@gmail.com>
2020-05-13 21:14:21 +0200

    feat: fix code smell

--- src/main/java/tech/gruppone/stalker/server/repositories/UserRepository.java
+++ src/main/java/tech/gruppone/stalker/server/repositories/UserRepository.java
@@ -9,21 +9,8 @@ import tech.gruppone.stalker.server.model.db.UserDao;
 
 public interface UserRepository extends ReactiveCrudRepository<UserDao,Long> {
 
-  // @Query("select * from Users")
-  // public Flux<UserDao> findAll();
-
   @Query("SELECT * from User where id = :id")
   public Mono<UserDao> findById(Long id);
-
-  // @Query("SELECT id, email, firstName, lastName, birthDate, createdDate FROM Users u, UserData d WHERE u.id = d.userId AND u.id = :id")
-  // public Mono<User> findById(Long id);
-
-  // @Query("select * from Users u where u.email = :email")
-  // public Mono<UserDao> findByEmail(String email);
-
-  // @Modifying
-  // @Query("UPDATE UserData u SET u.firstName = :firstName, u.lastName = :lastName, u.birthDate = :birthdate, u.lastModifiedDate = now() WHERE u.userId = :id")
-  // public Mono<Void> updateUser(String firstName, String lastName, LocalDate birthdate, Long id);
 
   @Modifying
   @Query("DELETE Users, UserData FROM Users INNER JOIN UserData WHERE Users.id = UserData.userId AND UserData.userId = :id")
