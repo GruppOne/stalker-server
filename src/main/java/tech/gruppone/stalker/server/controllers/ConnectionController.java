b3774729b9443efb80c8e749239265216b886bf1
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-05-13 12:41:09 +0200

    refactor: use conventions for naming

--- src/main/java/tech/gruppone/stalker/server/controllers/ConnectionController.java
+++ src/main/java/tech/gruppone/stalker/server/controllers/ConnectionController.java
@@ -2,42 +2,40 @@ package tech.gruppone.stalker.server.controllers;
 
 
 import java.time.LocalDateTime;
-import lombok.NonNull;
-import lombok.Value;
-import lombok.experimental.NonFinal;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
+
+import lombok.NonNull;
+import lombok.Value;
+import lombok.experimental.NonFinal;
 import reactor.core.publisher.Mono;
-import tech.gruppone.stalker.server.model.Connection;
 import tech.gruppone.stalker.server.repositories.ConnectionRepository;
 
 @RestController
 @Value
 @NonFinal
-public class UserConnectController {
+public class ConnectionController {
 
   @NonNull
   ConnectionRepository connectionRepository;
 
   @PostMapping("/user/{userId}/organization/{organizationId}/connection")
   @ResponseStatus(HttpStatus.CREATED)
-  Mono<Void> connectUserToOrganization(
+  public Mono<Void> createUserConnection(
     @PathVariable("userId") long userId, @PathVariable("organizationId") long organizationId
   ) {
-    // the service will manage LDAP properties if the organization is private
-    return connectionRepository.connectUserToOrganization(userId, organizationId, LocalDateTime.now());
+    // TOO manage LDAP properties if the organization is private
+    return connectionRepository.createUserConnection(userId, organizationId, LocalDateTime.now());
   }
 
   @DeleteMapping("/user/{userId}/organization/{organizationId}/connection")
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  Mono<Void> deleteUserConnectionToOrganization(@PathVariable("userId") long userId, @PathVariable("organizationId") long organizationId){
-    return connectionRepository.deleteUserConnectionToOrganization(userId, organizationId);
+ public Mono<Void> deleteUserConnectionToOrganization(@PathVariable("userId") long userId, @PathVariable("organizationId") long organizationId){
+    return connectionRepository.deleteUserConnection(userId, organizationId);
   }
 }
