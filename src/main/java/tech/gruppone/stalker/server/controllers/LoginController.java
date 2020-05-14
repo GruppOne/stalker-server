301e494e12b2a5941d254aa673dfd32959899a30
RiccardoCestaro <riccardo.cestaro@outlook.it>
2020-05-15 00:40:29 +0200

    fix: check formatting

--- src/main/java/tech/gruppone/stalker/server/controllers/LoginController.java
+++ src/main/java/tech/gruppone/stalker/server/controllers/LoginController.java
@@ -1,12 +1,11 @@
 package tech.gruppone.stalker.server.controllers;
 
+import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import lombok.Value;
 import reactor.core.publisher.Mono;
 import tech.gruppone.stalker.server.model.api.EncodedJwtDto;
 import tech.gruppone.stalker.server.model.api.LoginDataDto;
