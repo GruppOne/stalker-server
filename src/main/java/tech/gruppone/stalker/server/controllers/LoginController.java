a7e875dca7079e14eac814b5c36ba889356aa714
alberto1598 <albertococco98@gmail.com>
2020-05-14 16:38:49 +0200

    feat: add login feature which returns an example string

---
+++ src/main/java/tech/gruppone/stalker/server/controllers/LoginController.java
@@ -0,0 +1,27 @@
+package tech.gruppone.stalker.server.controllers;
+
+import lombok.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import tech.gruppone.stalker.server.model.api.EncodedJwtDto;
+import tech.gruppone.stalker.server.model.api.LoginDataDto;
+import tech.gruppone.stalker.server.services.LoginService;
+
+@RestController
+@Value
+public class LoginController {
+
+  LoginService loginService;
+
+
+  @PostMapping("/user/login")
+  @ResponseStatus(HttpStatus.CREATED)
+  Mono<EncodedJwtDto> logUser(@RequestBody LoginDataDto loginDataDto){
+    return loginService.logUser(loginDataDto);
+  }
+
+}
---
+++ src/main/java/tech/gruppone/stalker/server/model/api/EncodedJwtDto.java
@@ -0,0 +1,12 @@
+package tech.gruppone.stalker.server.model.api;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class EncodedJwtDto {
+
+  String encodedJwt;
+
+}
