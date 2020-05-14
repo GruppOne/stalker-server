4ecb00dc1808f64be28359c964fc13f39d754576
AlbertoGobbo <gobboalberto98@gmail.com>
2020-05-14 15:30:33 +0200

    feat: prepare new structure for /user/login

---
+++ src/main/java/tech/gruppone/stalker/server/model/api/UserRoleDto.java
@@ -0,0 +1,20 @@
+package tech.gruppone.stalker.server.model.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Column;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRoleDto {
+
+
+    @Column("organizationId")
+    private Integer organizationId;
+
+    private String role;
+}
