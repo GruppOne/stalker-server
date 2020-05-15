7d6cd4d7b06996ac70bc3f42f4bb89a4ca6785d1
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-05-15 04:42:43 +0200

    feat: implement get organizations and specific organization

--- src/main/java/tech/gruppone/stalker/server/controllers/OrganizationsController.java
+++ src/main/java/tech/gruppone/stalker/server/controllers/OrganizationsController.java
@@ -1,30 +1,47 @@
 package tech.gruppone.stalker.server.controllers;
 
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Value;
+import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import tech.gruppone.stalker.server.exceptions.NotImplementedException;
+import tech.gruppone.stalker.server.model.api.OrganizationDataDto;
 import tech.gruppone.stalker.server.model.api.OrganizationDto;
-import tech.gruppone.stalker.server.repositories.OrganizationRepository;
+import tech.gruppone.stalker.server.services.OrganizationService;
 
-@Value
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RestController
 @RequestMapping("/organizations")
 public class OrganizationsController {
 
-  OrganizationRepository organizationRepository;
+  OrganizationService organizationService;
 
   @GetMapping
-  public Mono<OrganizationsResponse> getOrganizations() {
-    // return organizationRepository.findAll().collectList().map(OrganizationsResponse::new);
+  public Mono<GetOrganizationsResponse> getOrganizations() {
+     return organizationService.findAll().collectList().map(GetOrganizationsResponse::new);
+  }
+
+  @PostMapping
+  public Mono<PostOrganizationsResponse> postOrganizations(
+      @RequestBody OrganizationDataDto organizationDataDto) {
     return Mono.error(NotImplementedException::new);
   }
 
   @Value
-  private static class OrganizationsResponse {
+  private static class GetOrganizationsResponse {
     List<OrganizationDto> organizations;
+  }
+
+  @Value
+  private static class PostOrganizationsResponse {
+    long id;
   }
 }
--- src/main/java/tech/gruppone/stalker/server/services/OrganizationService.java
+++ src/main/java/tech/gruppone/stalker/server/services/OrganizationService.java
@@ -6,7 +6,6 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import tech.gruppone.stalker.server.exceptions.NotImplementedException;
 import tech.gruppone.stalker.server.model.api.OrganizationDataDto;
 import tech.gruppone.stalker.server.model.api.OrganizationDataDto.OrganizationDataDtoBuilder;
 import tech.gruppone.stalker.server.model.api.OrganizationDto;
@@ -22,31 +21,28 @@ public class OrganizationService {
   PlaceService placeService;
 
   public Flux<OrganizationDto> findAll() {
-    // Flux<OrganizationDao> organizationDaos = organizationRepository.findAll();
-
-    // var organizationDataBuilder = OrganizationDataDto.builder();
-
-    //  Flux<OrganizationDto> organizationDtos = organizations.flatMap(organization -> {
-    //   Flux<PlaceDao> places = placeRepository.findByOrganizationId(organization.getId());
-    // });
-    return Flux.error(NotImplementedException::new);
+    // I have no idea why this works
+    return organizationRepository.findAll().map(OrganizationDao::getId).flatMap(this::findById);
   }
 
   public Mono<OrganizationDto> findById(final long id) {
-    final Mono<OrganizationDao> organizationDao = organizationRepository.findById(id);
     final Flux<PlaceDto> places = placeService.findAllByOrganizationId(id);
+    final Mono<OrganizationDao> organizationDao = organizationRepository.findById(id);
 
     final OrganizationDataDtoBuilder builder = OrganizationDataDto.builder();
+    places.subscribe(builder::place);
 
-    organizationDao.doOnNext(
-        orgDao ->
-            builder
-                .name(orgDao.getName())
-                .description(orgDao.getDescription())
-                .isPrivate(orgDao.isPrivate())
-                .creationDateTime(orgDao.getCreatedDate())
-                .lastChangeDateTime(orgDao.getLastModifiedDate()));
-
-    return Mono.error(NotImplementedException::new);
+    return places
+        .then(organizationDao)
+        .map(
+            orgDao ->
+                builder
+                    .name(orgDao.getName())
+                    .description(orgDao.getDescription())
+                    .isPrivate(orgDao.isPrivate())
+                    .creationDateTime(orgDao.getCreatedDate())
+                    .lastChangeDateTime(orgDao.getLastModifiedDate())
+                    .build())
+        .map(data -> new OrganizationDto(id, data));
   }
 }
