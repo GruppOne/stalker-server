14f22d08f509fcc5d6cc617231f5003a54c14077
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-05-15 21:07:49 +0200

    feat: start configuring cors

--- src/main/java/tech/gruppone/stalker/server/configuration/ApplicationConfiguration.java
+++ src/main/java/tech/gruppone/stalker/server/configuration/ApplicationConfiguration.java
@@ -5,16 +5,31 @@ import lombok.experimental.NonFinal;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 @lombok.Value
 @NonFinal
-public class ApplicationConfiguration {
+public class ApplicationConfiguration implements WebFluxConfigurer {
 
   String version;
 
   public ApplicationConfiguration(@NonNull @Value("${spring.application.version}") String version) {
     this.version = version;
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+
+    registry
+        .addMapping("/**")
+        // .allowedOrigins("https://gruppone.tech")
+        .allowedOrigins("http://localhost:4200")
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedHeaders("*")
+        .allowCredentials(true)
+        .maxAge(3600);
   }
 }
