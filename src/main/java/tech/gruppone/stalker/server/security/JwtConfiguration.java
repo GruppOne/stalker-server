58e552d88907bba3101345e48ca41962cccc193c
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-05-15 21:04:43 +0200

    refactor: standardize jwt configuration

--- src/main/java/tech/gruppone/stalker/server/security/JwtConfiguration.java
+++ src/main/java/tech/gruppone/stalker/server/security/JwtConfiguration.java
@@ -1,58 +1,77 @@
 package tech.gruppone.stalker.server.security;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;;
+
 import java.security.Key;
 import java.util.Date;
 import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.AccessLevel;
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @Configuration
 @PropertySource("classpath:application.properties")
-public class JwtConfiguration{
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class JwtConfiguration {
 
-  @Value("${spring.jwt.secret}")
-  private String secret;
+  String secret;
+  String expirationTime;
 
-  @Value("${spring.jwt.expirationtime}")
-  private String expirationTime;
+  public JwtConfiguration(
+      @NonNull @Value("${jwt.secret}") String secret,
+      @NonNull @Value("${jwt.expiration-time}") String expirationTime) {
+    this.secret = secret;
+    this.expirationTime = expirationTime;
+  }
 
-  private Key getEncodedKey(){
+  private Key getEncodedKey() {
     byte[] keyBytes = Decoders.BASE64.decode(this.secret);
     return Keys.hmacShaKeyFor(keyBytes);
   }
 
-  public Claims getJWTString(String token){
-    return Jwts.parserBuilder().setSigningKey(getEncodedKey()).build().parseClaimsJws(token).getBody();
+  // TODO should probably move the following methods to a separate class called JwtTokenService with an injected instance of jwtconfiguration
+
+  public Claims getJWTString(String token) {
+    return Jwts.parserBuilder()
+        .setSigningKey(getEncodedKey())
+        .build()
+        .parseClaimsJws(token)
+        .getBody();
   }
 
-  public Boolean isTokenSigned(String token){
+  public Boolean isTokenSigned(String token) {
     return Jwts.parserBuilder().setSigningKey(getEncodedKey()).build().isSigned(token);
   }
 
-  public String getUserId(String token){
+  public String getUserId(String token) {
     return getJWTString(token).getSubject();
   }
 
-  public Date getExpirationDate(String token){
+  public Date getExpirationDate(String token) {
     return getJWTString(token).getExpiration();
   }
 
-  public boolean isTokenExpired(String token){
+  public boolean isTokenExpired(String token) {
     Date date = new Date();
     return getExpirationDate(token).before(date);
   }
 
-
-  public String createToken(Long id){
+  public String createToken(Long id) {
     Date issuedAt = new Date();
-    Date expirationAt= new Date(issuedAt.getTime() + Long.parseLong(expirationTime));
-    return Jwts.builder().setSubject(String.valueOf(id)).setIssuedAt(issuedAt).setExpiration(expirationAt).setId(UUID.randomUUID().toString()).signWith(getEncodedKey()).compact();
+    Date expirationAt = new Date(issuedAt.getTime() + Long.parseLong(expirationTime));
+    return Jwts.builder()
+        .setSubject(String.valueOf(id))
+        .setIssuedAt(issuedAt)
+        .setExpiration(expirationAt)
+        .setId(UUID.randomUUID().toString())
+        .signWith(getEncodedKey())
+        .compact();
   }
-
-  }
-
-
+}
