94e094d3667ed54489d1bad8191ac2dc23cfbe6c
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-05-15 04:39:42 +0200

    refactor: standardize controller annotations

--- src/main/java/tech/gruppone/stalker/server/controllers/VersionController.java
+++ src/main/java/tech/gruppone/stalker/server/controllers/VersionController.java
@@ -1,26 +1,30 @@
 package tech.gruppone.stalker.server.controllers;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Value;
+import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import tech.gruppone.stalker.server.configuration.ApplicationConfiguration;
 
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RestController
 @RequestMapping("/version")
-@Value
 public class VersionController {
 
   ApplicationConfiguration applicationConfiguration;
 
   @GetMapping
-  public Mono<VersionResponseBody> getVersion() {
-    return Mono.just(applicationConfiguration.getVersion()).map(VersionResponseBody::new);
+  public Mono<GetVersionResponseBody> getVersion() {
+    return Mono.just(applicationConfiguration.getVersion()).map(GetVersionResponseBody::new);
   }
 
   @Value
-  private static class VersionResponseBody {
+  private static class GetVersionResponseBody {
     String version;
   }
 }
