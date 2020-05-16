4e949a27d5b9b10c9deb1b782a1144a125ffeb8f
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-05-16 02:57:39 +0200

    feat: implement post organizations

--- src/main/java/tech/gruppone/stalker/server/repositories/PlaceRepository.java
+++ src/main/java/tech/gruppone/stalker/server/repositories/PlaceRepository.java
@@ -1,5 +1,6 @@
 package tech.gruppone.stalker.server.repositories;
 
+import org.springframework.data.r2dbc.repository.Modifying;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
@@ -11,10 +12,19 @@ import tech.gruppone.stalker.server.model.db.PlaceDao;
 public interface PlaceRepository extends ReactiveCrudRepository<PlaceDao, Long> {
 
   @Query(
-      "SELECT Place.id AS id, Place.organizationId AS organizationId, Place.name AS NAME, ST_ASGEOJSON(POSITION) AS positionGeoJson, PlaceData.address, PlaceData.city, PlaceData.zipcode, PlaceData.state FROM Place, PlaceData WHERE Place.id = PlaceData.id AND Place.id = :id")
+      "SELECT Place.id AS id, Place.organizationId AS organizationId, Place.name AS NAME, ST_ASGEOJSON(position) AS positionGeoJson, PlaceData.address, PlaceData.city, PlaceData.zipcode, PlaceData.state FROM Place, PlaceData WHERE Place.id = PlaceData.id AND Place.id = :id")
   Mono<PlaceDao> findById(final long id);
 
   @Query(
-      "SELECT Place.id AS id, Place.organizationId AS organizationId, Place.name AS NAME, ST_ASGEOJSON(POSITION) AS positionGeoJson, PlaceData.address, PlaceData.city, PlaceData.zipcode, PlaceData.state FROM Place, PlaceData WHERE Place.id = PlaceData.id AND organizationId = :organizationId")
+      "SELECT Place.id AS id, Place.organizationId AS organizationId, Place.name AS NAME, ST_ASGEOJSON(position) AS positionGeoJson, PlaceData.address, PlaceData.city, PlaceData.zipcode, PlaceData.state FROM Place, PlaceData WHERE Place.id = PlaceData.id AND organizationId = :organizationId")
   Flux<PlaceDao> findAllByOrganizationId(final long organizationId);
+
+  // XXX EXTREMELY INEFFICIENT: does a separate query for each new place
+  // this will cause problems because it does not know if the place had already been created. should
+  // be fine after refactoring PlaceDao
+  @Modifying
+  @Query(
+      "INSERT INTO `Place`(`organizationId`, `name`, `position`) VALUES (:organizationId, :name, ST_GeomFromGeoJSON(:rawPositionJson))")
+  // handle errors by returning Mono<Integer>
+  Mono<Void> create(final Long organizationId, final String name, final String rawPositionJson);
 }
