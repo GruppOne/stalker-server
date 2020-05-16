279dcdefaa3bc9009823a20a4d3715cedafd5cac
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-05-16 04:15:58 +0200

    test: add some tests
    parametric tests are my secret kink

--- src/test/java/tech/gruppone/stalker/server/services/UserServiceTest.java
+++ src/test/java/tech/gruppone/stalker/server/services/UserServiceTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 import tech.gruppone.stalker.server.model.api.UserDataDto;
 import tech.gruppone.stalker.server.model.api.UserDto;
@@ -20,7 +19,6 @@ import tech.gruppone.stalker.server.repositories.UserRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserServiceTest {
-  @Autowired WebTestClient testClient;
   @MockBean UserDataRepository userDataRepository;
   @MockBean UserRepository userRepository;
   @Autowired UserService userService;
