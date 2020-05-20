7be987dbc8d5170c90871897a510db6fbca81126
AlbertoGobbo <gobboalberto98@gmail.com>
2020-05-20 17:03:50 +0200

    feat: add basic ldap authorization
    see SecurityConfiguration.java to check which endpoint is involved

--- src/main/java/tech/gruppone/stalker/server/configuration/SecurityConfiguration.java
+++ src/main/java/tech/gruppone/stalker/server/configuration/SecurityConfiguration.java
@@ -1,8 +1,40 @@
 package tech.gruppone.stalker.server.configuration;
 
-// @EnableWebFluxSecurity
-// @EnableReactiveMethodSecurity
-// @PropertySource("classpath:application.properties")
-class SecurityConfiguration {
-  // TODO configure spring security here
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@PropertySource("classpath:application.properties")
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .authorizeRequests()
+      .antMatchers("/test/ldap")
+      .fullyAuthenticated()
+      .and()
+      .formLogin();
+  }
+
+  @Override
+  public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+      .ldapAuthentication()
+        .userDnPatterns("uid={0},ou=people")
+        .groupSearchBase("ou=groups")
+        .contextSource()
+          .url("ldap://localhost:8389/dc=springframework,dc=org")
+          .and()
+        .passwordCompare()
+          .passwordEncoder(new BCryptPasswordEncoder())
+          .passwordAttribute("userPassword");
+  }
+
 }
