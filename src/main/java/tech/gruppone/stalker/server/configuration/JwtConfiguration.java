b8fbb9aad38716c88f28e83bc53d2771578ab5af
alberto1598 <albertococco98@gmail.com>
2020-05-21 11:13:41 +0200

    style: format code

--- src/main/java/tech/gruppone/stalker/server/configuration/JwtConfiguration.java
+++ src/main/java/tech/gruppone/stalker/server/configuration/JwtConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-
 @Configuration
 @PropertySource("classpath:application.properties")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -18,16 +17,15 @@ public class JwtConfiguration {
   String expirationTime;
 
   public JwtConfiguration(
-    @Value("${jwt.secret}") String encodedKey,
-    @Value("${jwt.expiration-time}") String expirationTime) {
+      @Value("${jwt.secret}") String encodedKey,
+      @Value("${jwt.expiration-time}") String expirationTime) {
     this.encodedKey = Base64.getEncoder().encodeToString(encodedKey.getBytes());
     this.expirationTime = expirationTime;
   }
 
-  public String getExpirationTime(){
-     return expirationTime;
+  public String getExpirationTime() {
+    return expirationTime;
   }
-
 
   public Key getEncodedKey() {
     return Keys.hmacShaKeyFor(encodedKey.getBytes());
