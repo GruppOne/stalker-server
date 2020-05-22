839f6eae9cabb52f66015405a13d65d2392eeb39
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-05-23 01:08:47 +0200

    refactor: cleanup controller and repository

--- src/main/java/tech/gruppone/stalker/server/repositories/OrganizationRoleRepository.java
+++ src/main/java/tech/gruppone/stalker/server/repositories/OrganizationRoleRepository.java
@@ -1,14 +1,12 @@
 package tech.gruppone.stalker.server.repositories;
 
-import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import tech.gruppone.stalker.server.model.db.OrganizationRole;
 
 public interface OrganizationRoleRepository extends ReactiveCrudRepository<OrganizationRole, Long> {
 
-  @Override
-  @Query("SELECT * FROM OrganizationRole")
-  public Flux<OrganizationRole> findAll();
+  // TODO check if this works without @query
+  Mono<OrganizationRole> findByOrganizationIdAndUserId(
+      final long organizationId, final long userId);
 }
