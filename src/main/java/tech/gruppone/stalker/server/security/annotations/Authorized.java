411dfe93fcac3a2487e46541a7e1f66022f17bca
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-05-22 19:59:00 +0200

    feat: add authorization annotation

---
+++ src/main/java/tech/gruppone/stalker/server/security/annotations/Authorized.java
@@ -0,0 +1,12 @@
+package tech.gruppone.stalker.server.security.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Authorized {
+  public int atMost() default 1;
+}
