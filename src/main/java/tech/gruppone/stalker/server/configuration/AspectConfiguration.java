c3825555198f74b9600a9fdf1c6701545b4defc3
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-05-22 19:58:29 +0200

    chore: add spring aop sonfiguration

---
+++ src/main/java/tech/gruppone/stalker/server/configuration/AspectConfiguration.java
@@ -0,0 +1,10 @@
+package tech.gruppone.stalker.server.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectConfiguration {
+
+}
