99416c0a867db9a9ac44a31ce8e2b1aeb70e358b
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-05-24 19:49:40 +0200

    feat: add spring security configuration

--- src/main/java/tech/gruppone/stalker/server/configuration/SecurityConfiguration.java
+++ src/main/java/tech/gruppone/stalker/server/configuration/SecurityConfiguration.java
@@ -1,8 +1,39 @@
 package tech.gruppone.stalker.server.configuration;
 
-// @EnableWebFluxSecurity
-// @EnableReactiveMethodSecurity
-// @PropertySource("classpath:application.properties")
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+@PropertySource("classpath:application.properties")
 class SecurityConfiguration {
-  // TODO configure spring security here
+  @Bean
+  protected SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http){
+    return http
+      //disable csrf
+      .csrf().disable()
+      //disable login form
+      .formLogin().disable()
+      //disable logout form
+      .logout().disable()
+      // disable basic authentication
+      .httpBasic().disable()
+      // setup permissions
+      .authorizeExchange()
+      //allow everyone to login
+      .pathMatchers(HttpMethod.POST, "/user/login").permitAll()
+      //allow everyone to signup
+      .pathMatchers(HttpMethod.POST, "/users").permitAll()
+      //allow everyone to read version
+      .pathMatchers(HttpMethod.GET, "/version").permitAll()
+      //require authentication for all other endpoints
+      .anyExchange().authenticated()
+      // complete the filter chain
+      .and().build();
+  }
 }
