721aef4adadeacca12edf755b8256d4209bd4012
alberto1598 <albertococco98@gmail.com>
2020-05-26 18:16:51 +0200

    fix: remove a wrong import

--- src/main/java/tech/gruppone/stalker/server/repositories/UserRepository.java
+++ src/main/java/tech/gruppone/stalker/server/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package tech.gruppone.stalker.server.repositories;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
-import tech.gruppone.stalker.server.model.api.responses.PostIdResponse;
 import tech.gruppone.stalker.server.model.db.UserDao;
 
 public interface UserRepository extends ReactiveCrudRepository<UserDao, Long> {
