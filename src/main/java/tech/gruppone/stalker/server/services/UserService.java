855f00641d0d27acd984f1afe8cd9269789a22a0
alberto1598 <albertococco98@gmail.com>
2020-05-27 19:03:56 +0200

    feat: add endpoint and write controller tests

--- src/main/java/tech/gruppone/stalker/server/services/UserService.java
+++ src/main/java/tech/gruppone/stalker/server/services/UserService.java
@@ -1,6 +1,7 @@
 package tech.gruppone.stalker.server.services;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -10,6 +11,7 @@ import tech.gruppone.stalker.server.exceptions.NotFoundException;
 import tech.gruppone.stalker.server.model.api.UserDataDto;
 import tech.gruppone.stalker.server.model.api.UserDto;
 import tech.gruppone.stalker.server.model.db.UserDao;
+import tech.gruppone.stalker.server.model.db.UserDataDao;
 import tech.gruppone.stalker.server.repositories.UserDataRepository;
 import tech.gruppone.stalker.server.repositories.UserRepository;
 
@@ -23,42 +25,49 @@ public class UserService {
 
   public Mono<UserDto> findById(final long userId) {
 
-    return userRepository
-        .findById(userId)
-        .zipWith(userDataRepository.findById(userId))
-        .map(
-            result -> {
-              final var t1 = result.getT1();
-              final var t2 = result.getT2();
-              return UserDto.builder()
-                  .id(t1.getId())
-                  .data(
-                      UserDataDto.builder()
-                          .email(t1.getEmail())
-                          .firstName(t2.getFirstName())
-                          .lastName(t2.getLastName())
-                          .birthDate(t2.getBirthDate())
-                          .creationDateTime(Timestamp.valueOf(t2.getLastModifiedDate()))
-                          .build())
-                  .build();
-            });
+    return userRepository.findById(userId).zipWith(userDataRepository.findById(userId)).map(result -> {
+      var t1 = result.getT1();
+      var t2 = result.getT2();
+      return UserDto.builder()
+        .id(t1.getId())
+        .data(UserDataDto.builder()
+          .email(t1.getEmail())
+          .firstName(t2.getFirstName())
+          .lastName(t2.getLastName())
+          .birthDate(t2.getBirthDate())
+          .creationDateTime(Timestamp.valueOf(t2.getLastModifiedDate()))
+          .build())
+        .build();
+    });
   }
 
   public Mono<Void> updatePassword(
       final String oldPassword, final String newPassword, final Long userId) {
 
-    return userRepository
-        .findById(userId)
-        .filter(userDao -> userDao.getPassword().equals(oldPassword) && !(newPassword.isBlank()))
-        .switchIfEmpty(Mono.error(new NotFoundException()))
-        .flatMap(
-            userDao ->
-                userRepository.save(
-                    UserDao.builder()
-                        .id(userDao.getId())
-                        .email(userDao.getEmail())
-                        .password(newPassword)
-                        .build()))
-        .then();
+    return userRepository.findById(userId)
+      .filter(userDao -> userDao.getPassword().equals(updatePasswordDto.getOldPassword())
+        && !(updatePasswordDto.getNewPassword().isBlank()))
+      .switchIfEmpty(Mono.error(new NotFoundException()))
+      .flatMap(userDao -> userRepository.save(UserDao.builder()
+        .id(userDao.getId())
+        .email(userDao.getEmail())
+        .password(updatePasswordDto.getNewPassword())
+        .build()))
+      .then();
+  }
+
+  public Mono<Void> putUserById(UserDataDto userDataDto, Long userId) {
+    return userRepository.findById(userId)
+      .filter(userDao -> userDao.getEmail().equals(userDataDto.getEmail()))
+      .switchIfEmpty(Mono.error(new NotFoundException()))
+      .and(userDataRepository.save(UserDataDao.builder()
+        .userId(userId)
+        .firstName(userDataDto.getFirstName())
+        .lastName(userDataDto.getLastName())
+        .birthDate(userDataDto.getBirthDate())
+        .createdDate(LocalDateTime.now())
+        .lastModifiedDate(LocalDateTime.now())
+        .build()))
+      .then();
   }
 }
