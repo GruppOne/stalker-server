ee7214d704b42177f620a84dcb1a4ac88d143fd5
alberto1598 <albertococco98@gmail.com>
2020-05-27 19:03:56 +0200

    feat: add endpoint and write controller tests

--- src/main/java/tech/gruppone/stalker/server/controllers/UserController.java
+++ src/main/java/tech/gruppone/stalker/server/controllers/UserController.java
@@ -1,18 +1,21 @@
 package tech.gruppone.stalker.server.controllers;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import reactor.core.publisher.Mono;
-import tech.gruppone.stalker.server.exceptions.NotImplementedException;
+import tech.gruppone.stalker.server.model.api.UpdatePasswordDto;
+import tech.gruppone.stalker.server.model.api.UserDataDto;
 import tech.gruppone.stalker.server.model.api.UserDto;
 import tech.gruppone.stalker.server.repositories.UserRepository;
 import tech.gruppone.stalker.server.services.UserService;
@@ -43,8 +46,15 @@ public class UserController {
 
   @PutMapping
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  public Mono<Void> putUserById(@PathVariable("userId") final Long userId) {
+  public Mono<Void> putUserById(@PathVariable("userId") final Long userId, @RequestBody UserDataDto userDataDto){
 
-    return Mono.error(NotImplementedException::new);
+    return userService.putUserById(userDataDto, userId);
+  }
+
+  @PutMapping("/password")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public Mono<Void> putUserByIdPassword(
+      @RequestBody UpdatePasswordDto updatePasswordDto, @PathVariable final Long userId) {
+    return userService.updatePassword(updatePasswordDto, userId);
   }
 }
--- src/test/java/tech/gruppone/stalker/server/controllers/UserControllerTest.java
+++ src/test/java/tech/gruppone/stalker/server/controllers/UserControllerTest.java
@@ -1,14 +1,24 @@
 package tech.gruppone.stalker.server.controllers;
 
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import reactor.core.publisher.Mono;
+import tech.gruppone.stalker.server.model.api.UserDataDto;
+import tech.gruppone.stalker.server.model.db.UserDao;
+import tech.gruppone.stalker.server.model.db.UserDataDao;
+import tech.gruppone.stalker.server.repositories.UserDataRepository;
 import tech.gruppone.stalker.server.repositories.UserRepository;
 import tech.gruppone.stalker.server.services.UserService;
 
@@ -18,9 +28,14 @@ class UserControllerTest {
 
   @Autowired WebTestClient webTestClient;
 
-  @MockBean UserService userService;
+  @MockBean
+  UserService userService;
 
-  @MockBean UserRepository userRepository;
+  @MockBean
+  UserRepository userRepository;
+
+  @MockBean
+  UserDataRepository userDataRepository;
 
   @Test
   void testGetUserById() {
@@ -43,14 +58,59 @@ class UserControllerTest {
   }
 
   @Test
-  void testPutUserById() {
-    final long userId = 1L;
+  public void testUpdatePassword() {
 
-    webTestClient
-        .put()
-        .uri("/user/{userId}", userId)
-        .exchange()
-        .expectStatus()
-        .isEqualTo(HttpStatus.NOT_IMPLEMENTED);
+    long userId = 1L;
+
+    UpdatePasswordDto updatePasswordDto = UpdatePasswordDto.builder().oldPassword("mela").newPassword("ciao").build();
+    UserDao userDao1 = UserDao.builder().id(userId).email("marioRossi@gmail.com").password("ciao").build();
+    UserDao userDao2 = UserDao.builder().id(userId).email("marioRossi@gmail.com").password("ciao").build();
+    testClient.put()
+      .uri("/user/{userId}/password", userId)
+      .body(Mono.just(updatePasswordDto), UpdatePasswordDto.class)
+      .exchange()
+      .expectStatus()
+      .isNoContent();
+
+    // when(userRepository.save(userDao)).thenReturn(userDao);
+
+    doReturn(Mono.just(userDao2)).when(userRepository).save(userDao2);
+    doReturn(Mono.just(userDao1)).when(userRepository).findById(userId);
+
+    verify(userService).updatePassword(updatePasswordDto, userId);
   }
+
+  @Test
+  public void testPutUserById() {
+
+    final Long userId = 1L;
+    final String firstname = "Marco";
+    final String lastname = "Rossi";
+    final String email = "mariorossi@hotmail";
+    final LocalDate birthdate = LocalDate.now();
+    final LocalDateTime localDateTime = LocalDateTime.now();
+
+    final UserDataDto userDataDto = UserDataDto.builder()
+      .firstName(firstname)
+      .lastName(lastname)
+      .email(email)
+      .birthDate(birthdate)
+      .creationDateTime(Timestamp.valueOf(localDateTime))
+      .build();
+
+    UserDao userDao = UserDao.builder().id(userId).email("mariorossi@hotmail.it").password("ciao").build();
+
+    UserDataDao userDataDao = UserDataDao.builder().userId(userId).firstName(firstname).lastName(lastname).birthDate(birthdate).createdDate(localDateTime).lastModifiedDate(localDateTime).build();
+
+    doReturn(Mono.just(userDao)).when(userRepository).findById(userId);
+    doReturn(Mono.just(userDataDao)).when(userDataRepository).save(userDataDao);
+
+    testClient.put()
+      .uri("/user/{userId}", userId)
+      .body(Mono.just(userDataDto), UserDataDto.class)
+      .exchange()
+      .expectStatus()
+      .isNoContent();
+  }
+
 }
