89d578990464d33f303aa1c78f3c2382e1289d7f
RiccardoCestaro <riccardo.cestaro@outlook.it>
2020-05-28 09:16:03 +0200

    refactor: check formatting

--- src/main/java/tech/gruppone/stalker/server/controllers/ConnectionController.java
+++ src/main/java/tech/gruppone/stalker/server/controllers/ConnectionController.java
@@ -1,5 +1,8 @@
 package tech.gruppone.stalker.server.controllers;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,10 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.experimental.FieldDefaults;
 import lombok.extern.log4j.Log4j2;
 import reactor.core.publisher.Mono;
 import tech.gruppone.stalker.server.model.api.LdapConfigurationDto;
@@ -42,7 +41,6 @@ public class ConnectionController {
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public Mono<Void> deleteUserConnectionToOrganization(
       @PathVariable("userId") long userId, @PathVariable("organizationId") long organizationId) {
-
 
     return connectionService.deleteUserConnection(userId, organizationId);
   }
