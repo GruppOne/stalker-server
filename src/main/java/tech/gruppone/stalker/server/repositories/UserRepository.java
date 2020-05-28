0d30fc39edb51f093f372bbec6ce1587688aec51
AlbertoGobbo <gobboalberto98@gmail.com>
2020-05-28 18:28:49 +0200

    feat: complete the last functionality

--- src/main/java/tech/gruppone/stalker/server/repositories/UserRepository.java
+++ src/main/java/tech/gruppone/stalker/server/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package tech.gruppone.stalker.server.repositories;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 import tech.gruppone.stalker.server.model.db.UserDao;
 
 public interface UserRepository extends ReactiveCrudRepository<UserDao, Long> {
   Mono<UserDao> findByEmail(final String email);
+
+  @Query(
+      "SELECT * FROM User WHERE email = :email; UPDATE User u SET u.password = :newHashedPassword WHERE email = :email")
+  public Mono<UserDao> findByEmailAndUpdatePassword(String email, String newHashedPassword);
 }
