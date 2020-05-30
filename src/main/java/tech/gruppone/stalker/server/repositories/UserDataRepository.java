90242d5df052aeb7f45695d4c27e12aa34d648db
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-05-30 15:50:20 +0200

    perf: simplify repositories

--- src/main/java/tech/gruppone/stalker/server/repositories/UserDataRepository.java
+++ src/main/java/tech/gruppone/stalker/server/repositories/UserDataRepository.java
@@ -1,10 +1,6 @@
 package tech.gruppone.stalker.server.repositories;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Mono;
 import tech.gruppone.stalker.server.model.db.UserDataDao;
 
-public interface UserDataRepository extends ReactiveCrudRepository<UserDataDao, Long> {
-
-  Mono<UserDataDao> findById(Long id);
-}
+public interface UserDataRepository extends ReactiveCrudRepository<UserDataDao, Long> {}
