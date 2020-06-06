0036db5c8485c02604661ba236e9adeb510c67f2
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-06-06 18:10:00 +0200

    feat: implement services

--- src/main/java/tech/gruppone/stalker/server/services/OrganizationService.java
+++ src/main/java/tech/gruppone/stalker/server/services/OrganizationService.java
@@ -1,72 +1,119 @@
 package tech.gruppone.stalker.server.services;
 
 import java.sql.Timestamp;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import tech.gruppone.stalker.server.exceptions.NotFoundException;
 import tech.gruppone.stalker.server.model.api.OrganizationDataDto;
-import tech.gruppone.stalker.server.model.api.OrganizationDataDto.OrganizationDataDtoBuilder;
 import tech.gruppone.stalker.server.model.api.OrganizationDto;
-import tech.gruppone.stalker.server.model.api.PlaceDataDto;
-import tech.gruppone.stalker.server.model.api.PlaceDto;
 import tech.gruppone.stalker.server.model.db.OrganizationDao;
+import tech.gruppone.stalker.server.model.db.PlaceDao;
 import tech.gruppone.stalker.server.repositories.OrganizationRepository;
+import tech.gruppone.stalker.server.repositories.PlaceRepository;
 
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Service
-@Log4j2
 public class OrganizationService {
+  Clock clock;
+
   OrganizationRepository organizationRepository;
-  PlaceService placeService;
+  PlaceRepository placeRepository;
+
+  // TODO will need to merge organization with ldapconfiguration when implementing private orgs
+  private OrganizationDto fromTuple(final Tuple2<OrganizationDao, List<Long>> tuple) {
+    final var organization = tuple.getT1();
+    final var placeIds = tuple.getT2();
+
+    final Long id = organization.getId();
+    final String name = organization.getName();
+    final String description = organization.getDescription();
+    final String organizationType = organization.getOrganizationType();
+    final Timestamp creationDateTime = Timestamp.valueOf(organization.getCreatedDate());
+    final Timestamp lastChangeDateTime = Timestamp.valueOf(organization.getLastModifiedDate());
+
+    final var organizationData =
+        OrganizationDataDto.builder()
+            .name(name)
+            .description(description)
+            .organizationType(organizationType)
+            .creationDateTime(creationDateTime)
+            .lastChangeDateTime(lastChangeDateTime)
+            .build();
+
+    return new OrganizationDto(id, organizationData, placeIds);
+  }
 
   public Flux<OrganizationDto> findAll() {
-    // I have no idea why this works
-    return organizationRepository.findAll().map(OrganizationDao::getId).flatMap(this::findById);
+    final var organizationDaos = organizationRepository.findAll();
+    final var placeDaos = placeRepository.findAll();
+
+    return organizationDaos
+        .flatMap(
+            organizationDao -> {
+              final Flux<Long> placeIds =
+                  placeDaos
+                      .filter(
+                          placeDao -> placeDao.getOrganizationId().equals(organizationDao.getId()))
+                      .map(PlaceDao::getId);
+
+              return Mono.just(organizationDao).zipWith(placeIds.collectList());
+            })
+        .map(this::fromTuple);
   }
 
   public Mono<OrganizationDto> findById(final long id) {
-    final Flux<PlaceDto> places = placeService.findAllByOrganizationId(id);
     final Mono<OrganizationDao> organizationDao = organizationRepository.findById(id);
+    final Flux<Long> placeIdsFlux =
+        placeRepository.findAllByOrganizationId(id).map(PlaceDao::getId);
 
-    final OrganizationDataDtoBuilder builder = OrganizationDataDto.builder();
-    places.subscribe(builder::place);
-
-    return places
-        .then(organizationDao)
-        .map(
-            orgDao ->
-                builder
-                    .name(orgDao.getName())
-                    .description(orgDao.getDescription())
-                    .isPrivate(orgDao.getIsPrivate())
-                    .creationDateTime(Timestamp.valueOf(orgDao.getCreatedDate()))
-                    .lastChangeDateTime(Timestamp.valueOf(orgDao.getLastModifiedDate()))
-                    .build())
-        .map(data -> new OrganizationDto(id, data));
+    return organizationDao.zipWith(placeIdsFlux.collectList()).map(this::fromTuple);
   }
 
-  public Mono<Long> save(OrganizationDataDto organizationDataDto) {
+  private OrganizationDao fromDataDtoWithoutDates(final OrganizationDataDto organizationDataDto) {
 
-    // TODO implement private orgs
-    // XXX careful: there can be multiple organizations with the same name!
-    String name = organizationDataDto.getName();
-    String description = organizationDataDto.getDescription();
-    // TODO we should not have to have an id for places not yet created
-    Flux<PlaceDataDto> placeDataDtos =
-        Flux.fromIterable(organizationDataDto.getPlaces()).map(PlaceDto::getData);
+    final String name = organizationDataDto.getName();
+    final String description = organizationDataDto.getDescription();
+    final String organizationType = organizationDataDto.getOrganizationType();
 
-    OrganizationDao newOrganizationDao =
-        OrganizationDao.builder().name(name).description(description).build();
+    return OrganizationDao.builder()
+        .name(name)
+        .description(description)
+        .organizationType(organizationType)
+        .build();
+  }
+
+  public Mono<Long> save(final OrganizationDataDto organizationDataDto) {
+
+    final OrganizationDao newOrganizationDao = fromDataDtoWithoutDates(organizationDataDto);
+    // TODO insert ldapconfiguration if needed
+
+    return organizationRepository.save(newOrganizationDao).map(OrganizationDao::getId);
+  }
+
+  public Mono<Void> updateById(final Long id, final OrganizationDataDto organizationDataDto) {
+
+    final OrganizationDao updatedOrganization =
+        fromDataDtoWithoutDates(organizationDataDto)
+            .withId(id)
+            .withLastModifiedDate(LocalDateTime.now(clock));
+    // TODO update ldapconfiguration if needed
 
     return organizationRepository
-        .save(newOrganizationDao)
-        .doOnNext(orgDao -> log.info("created organization {}", orgDao))
-        .map(OrganizationDao::getId)
-        .doOnNext(organizationId -> placeService.saveAll(placeDataDtos, organizationId));
+        .findById(id)
+        .switchIfEmpty(Mono.error(NotFoundException::new))
+        .flatMap(
+            oldOrganization ->
+                organizationRepository.save(
+                    updatedOrganization.withCreatedDate(oldOrganization.getCreatedDate())))
+        .then();
   }
 }
--- src/main/java/tech/gruppone/stalker/server/services/PlaceService.java
+++ src/main/java/tech/gruppone/stalker/server/services/PlaceService.java
@@ -1,95 +1,133 @@
 package tech.gruppone.stalker.server.services;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 import tech.gruppone.stalker.server.model.api.PlaceDataDto;
 import tech.gruppone.stalker.server.model.api.PlaceDataDto.GeographicalPoint;
 import tech.gruppone.stalker.server.model.api.PlaceDataDto.PlaceInfo;
 import tech.gruppone.stalker.server.model.api.PlaceDto;
 import tech.gruppone.stalker.server.model.db.PlaceDao;
+import tech.gruppone.stalker.server.repositories.OrganizationRepository;
 import tech.gruppone.stalker.server.repositories.PlaceRepository;
 
 @Service
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-@Log4j2
 public class PlaceService {
+  Clock clock;
+
+  OrganizationRepository organizationRepository;
   PlaceRepository placeRepository;
 
   PlacePositionService placePositionService;
 
-  public Flux<PlaceDao> saveAll(final Flux<PlaceDataDto> placeDataDtos, final long organizationId) {
-    log.info("saving some places for organization {}", organizationId);
+  private PlaceDto fromTuple(final Tuple2<PlaceDao, List<GeographicalPoint>> tuple) {
+    final PlaceDao placeDao = tuple.getT1();
+    final List<GeographicalPoint> geographicalPoints = tuple.getT2();
 
-    var placeDaos = placeDataDtos.flatMap(placeDataDto -> save(placeDataDto, organizationId));
+    final var id = placeDao.getId();
+    final var name = placeDao.getName();
+    final var color = placeDao.getColor();
+    final var maxConcurrentUsers = placeDao.getMaxConcurrentUsers();
 
-    placeDaos.subscribe(log::info);
-    return placeDaos;
-  }
+    final var address = placeDao.getAddress();
+    final var city = placeDao.getCity();
+    final var zipcode = placeDao.getZipcode();
+    final var state = placeDao.getState();
 
-  public Mono<PlaceDao> save(final PlaceDataDto placeDataDto, final long organizationId) {
-    final String name = placeDataDto.getName();
-    final String address = placeDataDto.getPlaceInfo().getAddress();
-    final String city = placeDataDto.getPlaceInfo().getCity();
-    final String zipcode = placeDataDto.getPlaceInfo().getZipcode();
-    final String state = placeDataDto.getPlaceInfo().getState();
+    final var placeInfo =
+        PlaceInfo.builder().address(address).city(city).zipcode(zipcode).state(state).build();
 
-    log.info("saving place {} for organization {}", name, organizationId);
-
-    final PlaceDao placeDao =
-        PlaceDao.builder()
-            .organizationId(organizationId)
+    final var placeDataDto =
+        PlaceDataDto.builder()
             .name(name)
-            .address(address)
-            .city(city)
-            .zipcode(zipcode)
-            .state(state)
+            .color(color)
+            .maxConcurrentUsers(maxConcurrentUsers)
+            .polygon(geographicalPoints)
+            .placeInfo(placeInfo)
             .build();
 
-    // save place position to separate table
+    return new PlaceDto(id, placeDataDto);
+  }
+
+  public Mono<PlaceDto> findById(final long id) {
+    return placeRepository
+        .findById(id)
+        .zipWith(placePositionService.findGeographicalPointsByPlaceId(id))
+        .map(this::fromTuple);
+  }
+
+  public Flux<PlaceDto> findAllByOrganizationId(final long id) {
+    return placeRepository
+        .findAllByOrganizationId(id)
+        // XXX not optimal; does 1 db call per place.
+        .flatMap(
+            place -> {
+              final var geographicalPoints =
+                  placePositionService.findGeographicalPointsByPlaceId(place.getId());
+
+              return Mono.just(place).zipWith(geographicalPoints);
+            })
+        .map(this::fromTuple);
+  }
+
+  private PlaceDao fromDataDto(final long organizationId, final PlaceDataDto placeDataDto) {
+
+    return PlaceDao.builder()
+        .organizationId(organizationId)
+        .name(placeDataDto.getName())
+        .color(placeDataDto.getColor())
+        .maxConcurrentUsers(placeDataDto.getMaxConcurrentUsers())
+        .address(placeDataDto.getPlaceInfo().getAddress())
+        .city(placeDataDto.getPlaceInfo().getCity())
+        .zipcode(placeDataDto.getPlaceInfo().getZipcode())
+        .state(placeDataDto.getPlaceInfo().getState())
+        .build();
+  }
+
+  public Mono<Long> save(final long organizationId, final PlaceDataDto placeDataDto) {
+
+    final var placeDao = fromDataDto(organizationId, placeDataDto);
     final List<GeographicalPoint> polygon = placeDataDto.getPolygon();
 
     return placeRepository
         .save(placeDao)
+        // save place position to separate table after place has been created successfully
         .doOnNext(
-            createdPlace -> placePositionService.savePlacePosition(createdPlace.getId(), polygon));
+            createdPlace ->
+                placePositionService
+                    .save(createdPlace.getId(), polygon)
+                    // update organization lastModified field after insert is successful
+                    .then(updateOrganizationLastModified(organizationId)))
+        .map(PlaceDao::getId);
   }
 
-  private Mono<PlaceDto> convertDaoToDto(final PlaceDao placeDao) {
-    final long id = placeDao.getId();
-    final String name = placeDao.getName();
+  public Mono<Void> updateById(
+      final long placeId, final long organizationId, final PlaceDataDto placeDataDto) {
 
-    final Mono<List<GeographicalPoint>> geographicalPoints =
-        placePositionService.findGeographicalPointsByPlaceId(id);
+    final var placeDao = fromDataDto(organizationId, placeDataDto).withId(placeId);
+    final var polygon = placeDataDto.getPolygon();
 
-    final PlaceInfo.PlaceInfoBuilder placeInfoBuilder =
-        PlaceInfo.builder()
-            .address(placeDao.getAddress())
-            .city(placeDao.getCity())
-            .zipcode(placeDao.getZipcode())
-            .state(placeDao.getState());
-
-    return geographicalPoints.map(
-        polygon -> {
-          var placeInfo = placeInfoBuilder.build();
-          var placeDataDto =
-              PlaceDataDto.builder().name(name).polygon(polygon).placeInfo(placeInfo).build();
-
-          return new PlaceDto(id, placeDataDto);
-        });
+    return placeRepository
+        .save(placeDao)
+        // save place position even if not modified
+        .then(placePositionService.save(placeId, polygon))
+        .then(updateOrganizationLastModified(organizationId));
   }
 
-  public Mono<PlaceDto> findById(final long id) {
-    return placeRepository.findById(id).flatMap(this::convertDaoToDto);
-  }
-
-  public Flux<PlaceDto> findAllByOrganizationId(final long id) {
-    return placeRepository.findAllByOrganizationId(id).flatMap(this::convertDaoToDto);
+  private Mono<Void> updateOrganizationLastModified(final Long organizationId) {
+    return organizationRepository
+        .findById(organizationId)
+        .map(organization -> organization.withLastModifiedDate(LocalDateTime.now(clock)))
+        .flatMap(organizationRepository::save)
+        .then();
   }
 }
