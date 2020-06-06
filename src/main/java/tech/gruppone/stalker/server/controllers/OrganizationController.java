12c4a8730a431a799ca35c5676ae44a7f3da5e34
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-06-06 16:32:12 +0200

    feat: implement controllers

--- src/main/java/tech/gruppone/stalker/server/controllers/OrganizationController.java
+++ src/main/java/tech/gruppone/stalker/server/controllers/OrganizationController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import tech.gruppone.stalker.server.exceptions.NotImplementedException;
 import tech.gruppone.stalker.server.model.api.OrganizationDto;
+import tech.gruppone.stalker.server.repositories.OrganizationRepository;
 import tech.gruppone.stalker.server.services.OrganizationService;
 
 @AllArgsConstructor
@@ -23,6 +25,7 @@ import tech.gruppone.stalker.server.services.OrganizationService;
 public class OrganizationController {
 
   OrganizationService organizationService;
+  OrganizationRepository organizationRepository;
 
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
@@ -32,14 +35,17 @@ public class OrganizationController {
 
   @PutMapping
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  public Mono<Void> putOrganizationById(@PathVariable final long id) {
-    return Mono.error(NotImplementedException::new);
+  public Mono<Void> putOrganizationById(
+      @PathVariable final long id, @RequestBody OrganizationDto organizationDto) {
+
+    return organizationService.updateById(id, organizationDto.getData());
   }
 
   @DeleteMapping
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public Mono<Void> deleteOrganizationById(@PathVariable final long id) {
-    return Mono.error(NotImplementedException::new);
+
+    return organizationRepository.deleteById(id);
   }
 
   @GetMapping("/users/inside")
--- src/main/java/tech/gruppone/stalker/server/controllers/PlacesController.java
+++ src/main/java/tech/gruppone/stalker/server/controllers/PlacesController.java
@@ -1,17 +1,23 @@
 package tech.gruppone.stalker.server.controllers;
 
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Value;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import tech.gruppone.stalker.server.exceptions.NotImplementedException;
+import tech.gruppone.stalker.server.model.api.PlaceDataDto;
+import tech.gruppone.stalker.server.model.api.PlaceDto;
+import tech.gruppone.stalker.server.services.PlaceService;
 
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -19,20 +25,38 @@ import tech.gruppone.stalker.server.exceptions.NotImplementedException;
 @RequestMapping("/organization/{organizationId}/places")
 public class PlacesController {
 
+  PlaceService placeService;
+
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
-  public Mono<Throwable> getOrganizationByIdPlaces(
+  public Mono<GetOrganizationByIdPlacesResponse> getOrganizationByIdPlaces(
       @PathVariable("organizationId") final long organizationId) {
 
-    return Mono.error(NotImplementedException::new);
+    return placeService
+        .findAllByOrganizationId(organizationId)
+        .collectList()
+        .map(GetOrganizationByIdPlacesResponse::new);
+  }
+
+  @Value
+  static class GetOrganizationByIdPlacesResponse {
+    List<PlaceDto> places;
   }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public Mono<Throwable> postOrganizationByIdPlaces(
-      @PathVariable("organizationId") final long organizationId) {
+  public Mono<PostOrganizationByIdPlacesResponse> postOrganizationByIdPlaces(
+      @PathVariable("organizationId") final long organizationId,
+      @RequestBody PlaceDataDto placeDataDto) {
 
-    return Mono.error(NotImplementedException::new);
+    return placeService
+        .save(organizationId, placeDataDto)
+        .map(PostOrganizationByIdPlacesResponse::new);
+  }
+
+  @Value
+  static class PostOrganizationByIdPlacesResponse {
+    long id;
   }
 
   @GetMapping("/report")
