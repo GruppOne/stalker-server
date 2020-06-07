c2f52f6ac4c5e6428098ab7e1c11502a5a8aeb20
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-06-07 21:48:51 +0200

    test: implement tests

--- src/test/java/tech/gruppone/stalker/server/controllers/PasswordControllerTest.java
+++ src/test/java/tech/gruppone/stalker/server/controllers/PasswordControllerTest.java
@@ -1,14 +1,19 @@
 package tech.gruppone.stalker.server.controllers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 import tech.gruppone.stalker.server.controllers.PasswordController.PostUserPasswordRecoveryRequestBody;
@@ -18,11 +23,14 @@ import tech.gruppone.stalker.server.repositories.UserRepository;
 
 @Tag("integrationTest")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// needed to create the javamailsender mockbean. ref: https://stackoverflow.com/a/41512008/13323696
+@TestPropertySource(properties = {"management.health.mail.enabled=false"})
 class PasswordControllerTest {
 
   @Autowired WebTestClient webTestClient;
 
   @MockBean UserRepository userRepository;
+  @MockBean JavaMailSender javaMailSender;
 
   @Test
   void testPutUserByIdPassword() {
@@ -57,7 +65,18 @@ class PasswordControllerTest {
 
   @Test
   void testPostUserPasswordRecovery() {
-    final String email = "email@email.email";
+
+    final var userId = 1L;
+    final var email = "email@email.email";
+    final var oldPassword = "oldPassword";
+
+    final var user = UserDao.builder().id(userId).email(email).password(oldPassword).build();
+
+    when(userRepository.findByEmail(email)).thenReturn(Mono.just(user));
+    when(userRepository.save(any())).thenReturn(Mono.just(user));
+
+    ArgumentCaptor<SimpleMailMessage> captor = ArgumentCaptor.forClass(SimpleMailMessage.class);
+
     final PostUserPasswordRecoveryRequestBody requestBody =
         new PostUserPasswordRecoveryRequestBody(email);
 
@@ -67,6 +86,14 @@ class PasswordControllerTest {
         .body(Mono.just(requestBody), PostUserPasswordRecoveryRequestBody.class)
         .exchange()
         .expectStatus()
-        .isEqualTo(HttpStatus.NOT_IMPLEMENTED);
+        .isNoContent();
+
+    verify(userRepository).findByEmail(email);
+    verify(userRepository).save(any());
+
+    verify(javaMailSender).send(captor.capture());
+
+    assertThat(captor.getValue().getTo()[0]).isEqualTo(email);
+    assertThat(captor.getValue().getText()).contains("Hi,");
   }
 }
