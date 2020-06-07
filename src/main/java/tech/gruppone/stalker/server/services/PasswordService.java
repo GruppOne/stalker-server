b21ca189563d6d10cb54841dc9ec68244397afef
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-06-07 21:47:52 +0200

    refactor: simplify resetPassword method

--- src/main/java/tech/gruppone/stalker/server/services/PasswordService.java
+++ src/main/java/tech/gruppone/stalker/server/services/PasswordService.java
@@ -3,83 +3,91 @@ package tech.gruppone.stalker.server.services;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Base64.Encoder;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
-import tech.gruppone.stalker.server.exceptions.InvalidEmailException;
 import tech.gruppone.stalker.server.repositories.UserRepository;
 
+@Log4j2
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Service
-@Log4j2
 public class PasswordService {
-
-  UserRepository userRepository;
 
   JavaMailSender javaMailSender;
 
-  public void sendEmail(String to, String subject, String body) throws MessagingException {
+  UserRepository userRepository;
 
-    MimeMessage message = javaMailSender.createMimeMessage();
+  public void resetPassword(final String userEmail) {
+    final String newPassword = generateRandomSecurePassword();
+    final String hashedNewPassword = hashPassword(newPassword);
 
-    MimeMessageHelper helper = new MimeMessageHelper(message, true);
+    userRepository
+        .findByEmail(userEmail)
+        .flatMap(user -> userRepository.save(user.withPassword(hashedNewPassword)))
+        .doOnSuccess(nullVariable -> sendEmail(userEmail, newPassword))
+        .subscribe();
+  }
 
-    helper.setSubject(subject);
-    helper.setTo(to);
-    helper.setText(body, true);
+  private static final String EMAIL_SUBJECT = "Stalker password recovery";
+  private static final String EMAIL_BODY_TEMPLATE =
+      String.join(
+          System.getProperty("line.separator"),
+          "Hi,",
+          "you're receiving this email because you asked to recover your Stalker account's password.",
+          "your old password was reset and the new password associated to your Stalker account is:",
+          "    %s",
+          "Remember to change it to a new password on your next login in Stalker.");
+
+  private void sendEmail(final String emailAddress, final String newPassword) {
+
+    final String expandedBody = String.format(EMAIL_BODY_TEMPLATE, newPassword);
+
+    final SimpleMailMessage message = new SimpleMailMessage();
+    message.setSubject(EMAIL_SUBJECT);
+    message.setTo(emailAddress);
+    message.setText(expandedBody);
+
+    log.info("email address: {}", Arrays.toString(message.getTo()));
+    log.info("expanded email body: {}", message.getText());
 
     javaMailSender.send(message);
   }
 
-  private static String encryptPassword(String input) throws NoSuchAlgorithmException {
+  private static String hashPassword(final String password) {
 
-    MessageDigest md = MessageDigest.getInstance("SHA-512");
-    byte[] messageDigest = md.digest(input.getBytes());
-    BigInteger no = new BigInteger(1, messageDigest);
+    MessageDigest md;
+
+    try {
+      md = MessageDigest.getInstance("SHA-512");
+    } catch (final NoSuchAlgorithmException e) {
+      throw new UnsupportedOperationException("this should never happen");
+    }
+
+    final byte[] messageDigest = md.digest(password.getBytes());
+    final BigInteger no = new BigInteger(1, messageDigest);
     String hashtext = no.toString(16);
     while (hashtext.length() < 32) hashtext = "0" + hashtext;
 
     return hashtext;
   }
 
-  public Mono<Void> sendPasswordRecoveryEmail(String userEmail) throws NoSuchAlgorithmException {
-    /*
-    FOLLOW THESE STEPS to work properly with this endpoint and to not get an MailAuthenticationException error:
-    Since we are using Gmail to send email, we need to enable "less secure access" on the Gmail account.
-    - Log out from all the Google accounts from the browser. Login in to the gmail account configured in application.properties.
-    - Then click on https://myaccount.google.com/lesssecureapps and turn on access for less secure apps.
-     - Alternately, if the link up doesn't work, go on https://accounts.google.com/b/0/DisplayUnlockCaptcha.
-    Now, you invoke /user/password/recovery endpoint and you'll receive an mail by GruppOne!
-    */
-    String newPassword = Long.toHexString(Double.doubleToLongBits(Math.random()));
+  // careful: not mockable
+  private static final SecureRandom secureRandom = new SecureRandom();
 
-    return userRepository
-        .findByEmailAndUpdatePassword(userEmail, encryptPassword(newPassword))
-        .filter(result -> result.getEmail().equals(userEmail))
-        .map(
-            result -> {
-              try {
-                sendEmail(
-                    userEmail,
-                    "Stalker Recovery Password",
-                    "Hi, you're receiving this email because you asked to recover your Stalker account's password.<br/> Now your old password has been resetted, and the new password associated to your Stalker account is: <b>"
-                        + newPassword
-                        + "</b>.<br/>You can change to a new password on your next login in Stalker.<br/><br/>The GruppOne team.");
-              } catch (MessagingException e) {
-                log.info(e.getStackTrace());
-              }
-              log.info("The mail has been sent correctly to {} ", userEmail);
-              return Mono.empty();
-            })
-        .switchIfEmpty(Mono.error(new InvalidEmailException()))
-        .then();
+  private String generateRandomSecurePassword() {
+    final byte[] bytes = new byte[16];
+    secureRandom.nextBytes(bytes);
+
+    final Encoder encoder = Base64.getUrlEncoder().withoutPadding();
+    return encoder.encodeToString(bytes);
   }
 }
