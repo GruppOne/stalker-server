6daafc4ac1a39d744903a06879e88ebd353dd67d
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-06-07 16:11:10 +0200

    test: implement last tests

--- src/test/java/tech/gruppone/stalker/server/controllers/OrganizationsControllerTest.java
+++ src/test/java/tech/gruppone/stalker/server/controllers/OrganizationsControllerTest.java
@@ -1,33 +1,61 @@
 package tech.gruppone.stalker.server.controllers;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import tech.gruppone.stalker.server.ApplicationTestConfiguration;
 import tech.gruppone.stalker.server.model.api.OrganizationDataDto;
 import tech.gruppone.stalker.server.model.db.OrganizationDao;
+import tech.gruppone.stalker.server.model.db.PlaceDao;
 import tech.gruppone.stalker.server.repositories.OrganizationRepository;
-import tech.gruppone.stalker.server.services.PlaceService;
+import tech.gruppone.stalker.server.repositories.PlaceRepository;
 
 @Tag("integrationTest")
+@Import(ApplicationTestConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrganizationsControllerTest {
   private static final LocalDateTime LOCAL_DATETIME = LocalDateTime.parse("2020-01-01T01:01:01.01");
 
-  @MockBean private OrganizationRepository organizationRepository;
-  @MockBean private PlaceService placeService;
+  @MockBean OrganizationRepository organizationRepository;
+  @MockBean PlaceRepository placeRepository;
 
-  @Autowired private WebTestClient webTestClient;
+  @Autowired WebTestClient webTestClient;
+
+  // set some common variables
+  final long organizationId = 1L;
+
+  // place related variables
+  final Long placeId = 11L;
+  final String placeName = "placeName";
+  final String color = "color";
+  final String address = "address";
+  final String city = "city";
+  final String zipcode = "zipcode";
+  final String state = "state";
+  final PlaceDao place =
+      PlaceDao.builder()
+          .id(placeId)
+          .organizationId(organizationId)
+          .name(placeName)
+          .color(color)
+          .maxConcurrentUsers(111)
+          .address(address)
+          .city(city)
+          .zipcode(zipcode)
+          .state(state)
+          .build();
+  final List<Long> placeIds = List.of(placeId);
 
   @Test
   void testGetOrganizations() {
@@ -45,8 +73,7 @@ class OrganizationsControllerTest {
             .build();
 
     when(organizationRepository.findAll()).thenReturn(Flux.just(organizationDao));
-    when(organizationRepository.findById(1L)).thenReturn(Mono.just(organizationDao));
-    when(placeService.findAllByOrganizationId(any(Long.class))).thenReturn(Flux.empty());
+    when(placeRepository.findAll()).thenReturn(Flux.just(place));
 
     webTestClient
         .get()
@@ -56,17 +83,18 @@ class OrganizationsControllerTest {
         .isOk()
         .expectBody()
         .jsonPath("$.organizations[0].id")
-        .isEqualTo(1L)
-        .jsonPath("$.organizations[0].data.places")
+        .isEqualTo(organizationId)
+        .jsonPath("$.organizations[0].data.name")
+        .isEqualTo(name)
+        .jsonPath("$.organizations[0].placeIds")
         .isArray();
 
     verify(organizationRepository).findAll();
-    verify(placeService).findAllByOrganizationId(1L);
+    verify(placeRepository).findAll();
   }
 
   @Test
   void testPostOrganizations() {
-    final long organizationId = 1L;
     final String name = "name";
     final String description = "description";
 
@@ -78,7 +106,6 @@ class OrganizationsControllerTest {
 
     when(organizationRepository.save(expectedOrganizationDao.withId(null)))
         .thenReturn(Mono.just(expectedOrganizationDao));
-    when(placeService.saveAll(any(), eq(organizationId))).thenReturn(Flux.empty());
 
     webTestClient
         .post()
@@ -92,6 +119,5 @@ class OrganizationsControllerTest {
         .isEqualTo(organizationId);
 
     verify(organizationRepository).save(expectedOrganizationDao.withId(null));
-    verify(placeService).saveAll(any(), eq(organizationId));
   }
 }
--- src/test/java/tech/gruppone/stalker/server/services/OrganizationServiceTest.java
+++ src/test/java/tech/gruppone/stalker/server/services/OrganizationServiceTest.java
@@ -1,21 +1,212 @@
 package tech.gruppone.stalker.server.services;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import tech.gruppone.stalker.server.ApplicationTestConfiguration;
+import tech.gruppone.stalker.server.model.api.OrganizationDataDto;
+import tech.gruppone.stalker.server.model.api.OrganizationDto;
+import tech.gruppone.stalker.server.model.db.OrganizationDao;
+import tech.gruppone.stalker.server.model.db.PlaceDao;
+import tech.gruppone.stalker.server.repositories.OrganizationRepository;
+import tech.gruppone.stalker.server.repositories.PlaceRepository;
+
+@Import(ApplicationTestConfiguration.class)
+@SpringBootTest
 public class OrganizationServiceTest {
-  // private static final LocalDateTime LOCAL_DATETIME =
-  // LocalDateTime.parse("2020-01-01T01:01:01.01");
 
-  // long userId = 1L;
-  // String email = "gruppone.swe@gmail.it";
-  // String firstName = "Gruppone";
-  // String lastName = "Swe";
-  // LocalDate birthdate = LocalDate.parse("1960-04-16");
+  @MockBean OrganizationRepository organizationRepository;
+  @MockBean PlaceRepository placeRepository;
 
-  // var userDao = UserDao.builder().id(userId).email(email).password("password").build();
-  // var userDataDao =
-  // UserDataDao.builder().userId(userId).firstName(firstName).lastName(lastName).birthDate(birthdate).createdDate(LOCAL_DATETIME).lastModifiedDate(LOCAL_DATETIME).build();
+  @Autowired OrganizationService organizationService;
 
-  // when(userRepository.findAllUsers(organizationId)).thenReturn(Flux.just(userDao));
-  // when(userDataRepository.findAllUserData(organizationId)).thenReturn(Flux.just(userDataDao));
-  // verify(userRepository).findAllUsers(organizationId);
-  // verify(userDataRepository).findAllUserData(organizationId);
+  private static final LocalDateTime LOCAL_DATETIME = LocalDateTime.parse("2020-01-01T01:01:01.01");
+
+  // set some common variables
+  final long organizationId = 1L;
+  final String name = "name";
+  final String description = "description";
+  final String organizationType = "organizationType";
+
+  // set some common place variables
+  final Long placeId = 11L;
+  final String placeName = "placeName";
+  final String color = "color";
+  final String address = "address";
+  final String city = "city";
+  final String zipcode = "zipcode";
+  final String state = "state";
+  final PlaceDao place =
+      PlaceDao.builder()
+          .id(placeId)
+          .organizationId(organizationId)
+          .name(placeName)
+          .color(color)
+          .maxConcurrentUsers(111)
+          .address(address)
+          .city(city)
+          .zipcode(zipcode)
+          .state(state)
+          .build();
+  final List<Long> placeIds = List.of(placeId);
+
+  @Test
+  void testFindAll() {
+
+    final var expectedOrganizationDataDto =
+        OrganizationDataDto.builder()
+            .name(name)
+            .description(description)
+            .organizationType(organizationType)
+            .creationDateTime(Timestamp.valueOf(LOCAL_DATETIME))
+            .lastChangeDateTime(Timestamp.valueOf(LOCAL_DATETIME))
+            .build();
+
+    final var expectedOrganizationDto =
+        OrganizationDto.builder()
+            .id(organizationId)
+            .data(expectedOrganizationDataDto)
+            .placeIds(placeIds)
+            .build();
+
+    final var organization =
+        OrganizationDao.builder()
+            .id(organizationId)
+            .name(name)
+            .description(description)
+            .organizationType(organizationType)
+            .createdDate(LOCAL_DATETIME)
+            .lastModifiedDate(LOCAL_DATETIME)
+            .build();
+
+    when(organizationRepository.findAll()).thenReturn(Flux.just(organization));
+    when(placeRepository.findAll()).thenReturn(Flux.just(place));
+
+    final var sut = organizationService.findAll();
+
+    StepVerifier.create(sut).expectNext(expectedOrganizationDto).verifyComplete();
+
+    verify(organizationRepository).findAll();
+    verify(placeRepository).findAll();
+  }
+
+  @Test
+  void testFindById() {
+
+    final var placeIds = List.of(placeId);
+
+    final var expectedOrganizationDataDto =
+        OrganizationDataDto.builder()
+            .name(name)
+            .description(description)
+            .organizationType(organizationType)
+            .creationDateTime(Timestamp.valueOf(LOCAL_DATETIME))
+            .lastChangeDateTime(Timestamp.valueOf(LOCAL_DATETIME))
+            .build();
+
+    final var expectedOrganizationDto =
+        OrganizationDto.builder()
+            .id(organizationId)
+            .data(expectedOrganizationDataDto)
+            .placeIds(placeIds)
+            .build();
+
+    final var organization =
+        OrganizationDao.builder()
+            .id(organizationId)
+            .name(name)
+            .description(description)
+            .organizationType(organizationType)
+            .createdDate(LOCAL_DATETIME)
+            .lastModifiedDate(LOCAL_DATETIME)
+            .build();
+
+    when(organizationRepository.findById(organizationId)).thenReturn(Mono.just(organization));
+    when(placeRepository.findAllByOrganizationId(organizationId)).thenReturn(Flux.just(place));
+
+    final var sut = organizationService.findById(organizationId);
+
+    StepVerifier.create(sut).expectNext(expectedOrganizationDto).verifyComplete();
+
+    verify(organizationRepository).findById(organizationId);
+    verify(placeRepository).findAllByOrganizationId(organizationId);
+  }
+
+  @Test
+  void testSave() {
+    final var organizationDataDto =
+        OrganizationDataDto.builder()
+            .name(name)
+            .description(description)
+            .organizationType(organizationType)
+            .build();
+
+    final var organizationDao =
+        OrganizationDao.builder()
+            .name(name)
+            .description(description)
+            .organizationType(organizationType)
+            .build();
+
+    final var newOrganizationId = 111L;
+
+    when(organizationRepository.save(organizationDao))
+        .thenReturn(
+            Mono.just(
+                organizationDao
+                    .withId(newOrganizationId)
+                    .withCreatedDate(LOCAL_DATETIME)
+                    .withLastModifiedDate(LOCAL_DATETIME)));
+
+    final var sut = organizationService.save(organizationDataDto);
+
+    StepVerifier.create(sut).expectNext(newOrganizationId).verifyComplete();
+
+    verify(organizationRepository).save(organizationDao);
+  }
+
+  @Test
+  void testUpdateById() {
+    final var newName = "newName";
+    final var newDescription = "newDescription";
+
+    final var organizationDataDto =
+        OrganizationDataDto.builder()
+            .name(newName)
+            .description(newDescription)
+            .organizationType(organizationType)
+            .creationDateTime(Timestamp.valueOf(LOCAL_DATETIME))
+            .lastChangeDateTime(Timestamp.valueOf(LOCAL_DATETIME))
+            .build();
+
+    final var updatedOrganization =
+        OrganizationDao.builder()
+            .id(organizationId)
+            .name(newName)
+            .description(newDescription)
+            .organizationType(organizationType)
+            .createdDate(LOCAL_DATETIME)
+            .lastModifiedDate(ApplicationTestConfiguration.FIXED_LOCAL_DATE_TIME)
+            .build();
+
+    when(organizationRepository.save(updatedOrganization))
+        .thenReturn(Mono.just(updatedOrganization));
+
+    final var sut = organizationService.updateById(organizationId, organizationDataDto);
+
+    StepVerifier.create(sut).verifyComplete();
+
+    verify(organizationRepository).save(updatedOrganization);
+  }
 }
