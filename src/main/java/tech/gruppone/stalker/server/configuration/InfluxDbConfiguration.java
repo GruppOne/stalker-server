047b5f3c467fc9acdefdbc89a6e39807d39698c5
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-06-10 16:44:23 +0200

    feat: update connection and measurement schema

--- src/main/java/tech/gruppone/stalker/server/configuration/InfluxDbConfiguration.java
+++ src/main/java/tech/gruppone/stalker/server/configuration/InfluxDbConfiguration.java
@@ -1,76 +1,62 @@
 package tech.gruppone.stalker.server.configuration;
 
 import java.util.concurrent.TimeUnit;
-import lombok.NonNull;
-import lombok.experimental.NonFinal;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.log4j.Log4j2;
 import okhttp3.OkHttpClient;
-import org.influxdb.BatchOptions;
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDB.LogLevel;
-import org.influxdb.InfluxDBFactory;
 import org.influxdb.impl.InfluxDBMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.influx.InfluxDbOkHttpClientBuilderProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.PropertySource;
 
 @Log4j2
 @Configuration
 @PropertySource("classpath:application.properties")
-@lombok.Value
-@NonFinal
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class InfluxDbConfiguration {
 
-  String url;
-  String database;
-  String retentionPolicy;
-  String username;
-  String password;
+  @Getter String database;
+  @Getter String retentionPolicy;
 
+  // XXX fails silently when influxdb connection cannot be established
   public InfluxDbConfiguration(
-      @NonNull @Value("${influxdb.url}") String url,
-      @NonNull @Value("${influxdb.database}") String database,
-      @NonNull @Value("${influxdb.retention}") String retentionPolicy,
-      @NonNull @Value("${influxdb.user}") String userName,
-      @NonNull @Value("${influxdb.password}") String password) {
-    this.url = url;
+      @Value("${influxdb.database}") String database,
+      @Value("${influxdb.retention}") String retentionPolicy) {
     this.database = database;
     this.retentionPolicy = retentionPolicy;
-    this.username = userName;
-    this.password = password;
-  }
-
-  // FIXME fails silently when connection cannot be established
-  // TODO handle errors when establishing the connection. maybe add spring retry?
-  @Bean(name = "influxDbConnection", destroyMethod = "close")
-  public InfluxDB getInfluxDBConnection() {
-    log.info("creating connection to InfluxDB {} on db {}", url, database);
-
-    final OkHttpClient.Builder httpClientBuilder =
-        new OkHttpClient.Builder()
-            .connectTimeout(1, TimeUnit.MINUTES)
-            .retryOnConnectionFailure(false);
-
-    final InfluxDB influxDB =
-        InfluxDBFactory.connect(url, username, password, httpClientBuilder)
-            .setDatabase(database)
-            .setRetentionPolicy(retentionPolicy)
-            .enableGzip()
-            .setLogLevel(LogLevel.FULL)
-            .enableBatch(BatchOptions.DEFAULTS.precision(TimeUnit.MILLISECONDS));
-
-    log.info(influxDB);
-    // TODO how to catch errors here?
-    // influxDB.ping();
-
-    return influxDB;
   }
 
   @Bean
-  @DependsOn("influxDbConnection")
-  public InfluxDBMapper getInfluxDBMapper(InfluxDB influxDB) {
-    return new InfluxDBMapper(influxDB);
+  InfluxDbOkHttpClientBuilderProvider influxDbOkHttpClientBuilderProvider() {
+    log.info("configuring InfluxDB OkHttpClient client");
+
+    return () ->
+        new OkHttpClient.Builder()
+            .retryOnConnectionFailure(false)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS);
+  }
+
+  @Bean
+  public InfluxDBMapper influxDBMapper(InfluxDB influxDb) {
+    // this isn't the most appropriate place to configure this stuff
+    log.info("configuring InfluxDB connection");
+    influxDb
+        .setDatabase(database)
+        .setRetentionPolicy(retentionPolicy)
+        // .enableGzip()
+        .enableBatch()
+        // .enableBatch(BatchOptions.DEFAULTS.precision(TimeUnit.MILLISECONDS))
+        .setLogLevel(LogLevel.FULL);
+
+    log.info("instantiating InfluxDBMapper bean");
+    return new InfluxDBMapper(influxDb);
   }
 }
