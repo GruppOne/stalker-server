9f8a192d2d7da27dd8b1f9385a2e5a047025174c
AlbertoGobbo <gobboalberto98@gmail.com>
2020-06-12 02:37:10 +0200

    feat: change LdapConfiguration SQL table and add test data

--- db/relational/create.sql
+++ db/relational/create.sql
@@ -73,9 +73,8 @@ CREATE TABLE `Connection` (
 CREATE TABLE `LdapConfiguration` (
   `organizationId` int(11) NOT NULL,
   `url` varchar(250) NOT NULL,
-  `searchQuery` varchar(250) NOT NULL,
-  `bindDn` varchar(250) NOT NULL,
-  `bindPassword` varchar(250) NOT NULL
+  `username` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 -- --------------------------------------------------------
