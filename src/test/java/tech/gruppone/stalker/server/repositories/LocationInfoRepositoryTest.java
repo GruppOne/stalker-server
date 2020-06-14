4a603e6fcffa033a95b5b52ef1816368e238900b
Luca Ercole <30255227+erclu@users.noreply.github.com>
2020-06-14 22:47:53 +0200

    test: implement tests

--- src/test/java/tech/gruppone/stalker/server/repositories/LocationInfoRepositoryTest.java
+++ src/test/java/tech/gruppone/stalker/server/repositories/LocationInfoRepositoryTest.java
@@ -1,10 +1,14 @@
 package tech.gruppone.stalker.server.repositories;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.influxdb.querybuilder.BuiltQuery.QueryBuilder.desc;
+import static org.influxdb.querybuilder.BuiltQuery.QueryBuilder.eq;
+import static org.influxdb.querybuilder.BuiltQuery.QueryBuilder.select;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +17,6 @@ import org.influxdb.dto.Point;
 import org.influxdb.dto.Query;
 import org.influxdb.dto.QueryResult;
 import org.influxdb.impl.InfluxDBMapper;
-import org.influxdb.querybuilder.BuiltQuery.QueryBuilder;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +27,7 @@ import org.springframework.context.annotation.Import;
 import reactor.test.StepVerifier;
 import reactor.util.function.Tuples;
 import tech.gruppone.stalker.server.configuration.InfluxDbConfiguration;
+import tech.gruppone.stalker.server.model.api.UserHistoryPerOrganizationDto.PlaceHistoryDto;
 import tech.gruppone.stalker.server.model.db.LocationInfo;
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE, classes = LocationInfoRepository.class)
@@ -84,11 +88,11 @@ class LocationInfoRepositoryTest {
     final var lastStatus = true;
 
     final Query query =
-        QueryBuilder.select("inside")
+        select("inside")
             .from("stalker-tsdb", "access_log")
-            .where(QueryBuilder.eq("user_id", userId))
-            .and(QueryBuilder.eq("place_id", placeId))
-            .orderBy(QueryBuilder.desc())
+            .where(eq("user_id", userId))
+            .and(eq("place_id", placeId))
+            .orderBy(desc())
             .limit(1);
 
     final var series = new QueryResult.Series();
@@ -112,11 +116,11 @@ class LocationInfoRepositoryTest {
   @Test
   void testFindLastStatusByUserIdAndPlaceIdWhenNoPreviousStatus() {
     final Query query =
-        QueryBuilder.select("inside")
+        select("inside")
             .from("stalker-tsdb", "access_log")
-            .where(QueryBuilder.eq("user_id", userId))
-            .and(QueryBuilder.eq("place_id", placeId))
-            .orderBy(QueryBuilder.desc())
+            .where(eq("user_id", userId))
+            .and(eq("place_id", placeId))
+            .orderBy(desc())
             .limit(1);
 
     final var result = new QueryResult.Result();
@@ -142,34 +146,37 @@ class LocationInfoRepositoryTest {
     final long userId1 = 11L;
     final long userId2 = 12L;
 
-    final boolean lastStatus1 = false;
+    final boolean lastStatus1 = true;
     final boolean lastStatus2 = true;
 
     final Query query =
-        QueryBuilder.select()
+        select()
             .column("inside")
             .from("stalker-tsdb", "\"stalker-tsdb\".\"infinite\".\"access_log\"")
-            .where(QueryBuilder.eq("organization_id", String.valueOf(organizationIdLong)))
+            .where(eq("organization_id", String.valueOf(organizationIdLong)))
             .groupBy("place_id", "user_id")
-            .orderBy(QueryBuilder.desc())
+            .orderBy(desc())
             .limit(1);
 
     final var series1 = new QueryResult.Series();
+    final var series1bis = new QueryResult.Series();
     final var series2 = new QueryResult.Series();
     final var result = new QueryResult.Result();
     final QueryResult queryResult = new QueryResult();
 
-    series1.setColumns(List.of("time", "inside"));
     series1.setTags(
         Map.of("place_id", String.valueOf(placeId1), "user_id", String.valueOf(userId1)));
     series1.setValues(List.of(List.of("unused", lastStatus1)));
 
-    series2.setColumns(List.of("inside"));
+    series1bis.setTags(
+        Map.of("place_id", String.valueOf(placeId1), "user_id", String.valueOf(userId2)));
+    series1bis.setValues(List.of(List.of("unused", !lastStatus1)));
+
     series2.setTags(
         Map.of("place_id", String.valueOf(placeId2), "user_id", String.valueOf(userId2)));
     series2.setValues(List.of(List.of("unused", lastStatus2)));
 
-    result.setSeries(List.of(series1, series2));
+    result.setSeries(List.of(series1, series1bis, series2));
     queryResult.setResults(List.of(result));
 
     when(influxDB.query(query)).thenReturn(queryResult);
@@ -177,7 +184,7 @@ class LocationInfoRepositoryTest {
     final var sut = locationInfoRepository.findByOrganizationIdGroupByPlaceId(organizationIdLong);
 
     StepVerifier.create(sut)
-        .expectNext(Tuples.of(placeId1, 0))
+        .expectNext(Tuples.of(placeId1, 1))
         .expectNext(Tuples.of(placeId2, 1))
         .verifyComplete();
 
@@ -189,12 +196,12 @@ class LocationInfoRepositoryTest {
     final long organizationIdLong = 1L;
 
     final Query query =
-        QueryBuilder.select()
+        select()
             .column("inside")
             .from("stalker-tsdb", "\"stalker-tsdb\".\"infinite\".\"access_log\"")
-            .where(QueryBuilder.eq("organization_id", String.valueOf(organizationIdLong)))
+            .where(eq("organization_id", String.valueOf(organizationIdLong)))
             .groupBy("place_id", "user_id")
-            .orderBy(QueryBuilder.desc())
+            .orderBy(desc())
             .limit(1);
 
     final var result = new QueryResult.Result();
@@ -210,5 +217,69 @@ class LocationInfoRepositoryTest {
     StepVerifier.create(sut).verifyComplete();
 
     verify(influxDB).query(query);
+  }
+
+  @Test
+  void testFindHistoryByOrganizationIdAndUserId() {
+
+    final var organizationIdLong = 1L;
+    final var userIdLong = 11L;
+
+    final Query query =
+        select()
+            .from("stalker-tsdb", "\"stalker-tsdb\".\"infinite\".\"access_log\"")
+            .where(eq("organization_id", String.valueOf(organizationIdLong)))
+            .and(eq("user_id", String.valueOf(userIdLong)))
+            .orderBy(desc());
+
+    final var placeId1 = "11";
+    final var placeId2 = "12";
+    final var inside1 = true;
+    final var inside2 = false;
+
+    final LocationInfo model1 =
+        LocationInfo.builder()
+            .time(INSTANT)
+            .userId(String.valueOf(userId))
+            .placeId(placeId1)
+            .organizationId(String.valueOf(organizationId))
+            .anonymous(false)
+            .inside(inside1)
+            .build();
+
+    final var model2 =
+        LocationInfo.builder()
+            .time(INSTANT)
+            .userId(String.valueOf(userId))
+            .placeId(placeId2)
+            .organizationId(String.valueOf(organizationId))
+            .anonymous(false)
+            .inside(inside2)
+            .build();
+
+    when(influxDBMapper.query(query, LocationInfo.class)).thenReturn(List.of(model1, model2));
+
+    final var expectedPlaceHistoryDto1 =
+        PlaceHistoryDto.builder()
+            .timestamp(Timestamp.from(INSTANT))
+            .inside(inside1)
+            .placeId(Long.valueOf(placeId1))
+            .build();
+
+    final var expectedPlaceHistoryDto2 =
+        PlaceHistoryDto.builder()
+            .timestamp(Timestamp.from(INSTANT))
+            .inside(inside2)
+            .placeId(Long.valueOf(placeId2))
+            .build();
+
+    final var sut = locationInfoRepository.findHistoryByOrganizationIdAndUserId(null, null);
+
+    StepVerifier.create(sut)
+        .expectNext(expectedPlaceHistoryDto1)
+        .expectNext(expectedPlaceHistoryDto2)
+        .verifyComplete();
+
+    verify(influxDBMapper).query(query, LocationInfo.class);
   }
 }
