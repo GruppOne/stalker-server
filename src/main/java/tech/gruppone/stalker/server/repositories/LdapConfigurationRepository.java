77cb43276bd2e18cb97f8d6f7138cc9fac0b41d9
AlbertoGobbo <gobboalberto98@gmail.com>
2020-06-14 14:29:04 +0200

    feat: update definitively POST /organizations
    LDAP works.

--- src/main/java/tech/gruppone/stalker/server/repositories/LdapConfigurationRepository.java
+++ src/main/java/tech/gruppone/stalker/server/repositories/LdapConfigurationRepository.java
@@ -1,17 +1,7 @@
 package tech.gruppone.stalker.server.repositories;
 
-import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Mono;
 import tech.gruppone.stalker.server.model.db.LdapConfigurationDao;
 
 public interface LdapConfigurationRepository
-    extends ReactiveCrudRepository<LdapConfigurationDao, Long> {
-
-  // The save method has been overridden because this method invoked by save(organizationDataDto)
-  // produces only UPDATE query, but not INSERT query
-  @Query(
-      "INSERT INTO LdapConfiguration (organizationId, url, baseDn, bindDn, bindPassword) VALUES(:organizationId, :url, :baseDn, :bindDn, :bindPassword)")
-  Mono<LdapConfigurationDao> save(
-      Long organizationId, String url, String baseDn, String bindDn, String bindPassword);
-}
+    extends ReactiveCrudRepository<LdapConfigurationDao, Long> {}
