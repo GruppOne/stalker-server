0c24fda31cd61cae5da889f65a5ed12763ec4354
AlbertoGobbo <gobboalberto98@gmail.com>
2020-06-14 03:02:51 +0200

    feat: add ldap in POST /organizations
    one FIXME

--- src/main/java/tech/gruppone/stalker/server/model/api/OrganizationDataDto.java
+++ src/main/java/tech/gruppone/stalker/server/model/api/OrganizationDataDto.java
@@ -29,7 +29,7 @@ public class OrganizationDataDto {
 
     @NonNull String baseDn;
 
-    @NonNull String bindCn;
+    @NonNull String bindDn;
 
     @NonNull String bindPassword;
   }
--- src/main/java/tech/gruppone/stalker/server/model/db/LdapConfigurationDao.java
+++ src/main/java/tech/gruppone/stalker/server/model/db/LdapConfigurationDao.java
@@ -1,14 +1,17 @@
 package tech.gruppone.stalker.server.model.db;
 
+import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
+@Builder
 @Value
 @Table("LdapConfiguration")
 public class LdapConfigurationDao {
+
   @Id
   @Column("organizationId")
   Long organizationId;
